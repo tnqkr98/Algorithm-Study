6acc07f3ded8afacfc432573db50b3c706012a39
donghan <tnqkr98@naver.com>
2020-03-30 23:50:33 +0900

    first commit

---
+++ baek1927(heap).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#pragma warning(disable:4996)
+using namespace std;
+int main() {
+	priority_queue<int, vector<int>, greater<int>> pq; 
+	vector<int>v;
+	int n, t;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &t);
+		if (t == 0 && !pq.empty()) {
+			v.push_back(pq.top());
+			pq.pop();
+		}
+		else if (t != 0)
+			pq.push(t);
+		else if (pq.empty())
+			v.push_back(0);
+	}
+	for (int i = 0; i < v.size(); i++)
+		printf("%d\n", v[i]);
+	return 0;
+}
