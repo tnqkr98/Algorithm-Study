0dcf1024dc043b9e4f8b676a655b294ea1a587ae
donghan <tnqkr98@naver.com>
2020-09-23 21:52:48 +0900

    dijkstra algorithm

---
+++ baek1916(최소비용구하기).cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <climits>
+#define INF INT_MAX
+#define MIN(a,b) (((a)>(b))?(b):(a))
+using namespace std;
+
+struct NW {
+	int node, wgt;
+};
+
+struct cmp{
+	bool operator()(NW a, NW b) {
+		if (a.wgt > b.wgt)		// 가중치 오름차순
+			return true;
+		else if (a.wgt == b.wgt) 
+			return a.node > b.node;	//노드번호 오름차순
+		return false;
+	}
+};
+
+int dist[1001];
+vector<NW> graph[1001];
+bool visit[1001];
+priority_queue<NW, vector<NW>, cmp> pq;
+
+int dijkstra(int start,int end) {
+	dist[start] = 0;
+	pq.push({ start,0 });
+	while (!pq.empty()) {
+		NW pick = pq.top();
+		pq.pop();
+		if (visit[pick.node]) continue;
+		visit[pick.node] = 1;
+
+		for (NW near : graph[pick.node]) {
+			if (!visit[near.node]) {
+				int before = dist[near.node];
+				dist[near.node] = MIN(dist[near.node], dist[pick.node] + near.wgt);
+				if (dist[near.node] != before)
+					pq.push({ near.node,dist[near.node] });
+			}
+		}
+	}
+	return dist[end];
+}
+
+int main() {
+	int n, m,i;
+	cin >> n >> m;
+
+	for (i = 0; i < m; i++) {
+		int s, e, w;
+		cin >> s >> e >> w;
+		graph[s].push_back({ e,w });
+	}
+
+	for (i = 0; i < n; i++) {
+		dist[i + 1] = INF;
+		visit[i + 1] = 0;
+	}
+
+	int start, end;
+	cin >> start >> end;
+
+	cout<<dijkstra(start, end);
+	
+	return 0;
+}
