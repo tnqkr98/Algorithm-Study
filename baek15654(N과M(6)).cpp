4c78ccf5f1acacb02e4e3184012c9a21198281a9
donghan <tnqkr98@naver.com>
2020-10-20 21:10:35 +0900

    n and m backtracking

---
+++ baek15654(N과M(6)).cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <string>
+#pragma warning(disable:4996)
+using namespace std;
+vector<int> v;
+//bool visit[9] = { 0 };
+int n, m;
+void back(string s,int d,int p) {
+	if (d == m) {
+		s.pop_back();
+		printf("%s\n", s.c_str());
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		//if (!visit[i] && v[i]>p) {
+			//visit[i] = 1;
+			back(s + to_string(v[i]) + " ", d + 1,v[i]);
+			//visit[i] = 0;
+		//}
+	}
+}
+int main() {
+	scanf("%d %d", &n,&m);
+	for (int i = 0; i < n; i++) {
+		int t;
+		scanf("%d", &t);
+		v.push_back(t);
+	}
+	sort(v.begin(), v.end());
+	back("",0,0);
+	return 0;
+}
