8f383da278ba48d10ad5d4f8f6a17f44ac1a68e7
donghan <tnqkr98@naver.com>
2021-08-20 22:37:51 +0900

    baek10989

---
+++ baek10989(수 정렬하기 3).cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#pragma warning(disable:4996)
+using namespace std;
+int main() {
+	int arr[10001] = { 0 }, n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int t;
+		scanf("%d", &t);
+		arr[t]++;
+	}
+	for (int i = 1; i < 10001; i++) {
+		if (arr[i] == 0) continue;
+		else
+			for (int j = arr[i]; j > 0; j--)
+				printf("%d\n", i);
+	}
+	return 0;
+}
