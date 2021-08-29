332eff564aeb56328fe9345d52967ba3d0e94100
donghan <tnqkr98@naver.com>
2021-08-29 22:22:23 +0900

    baek11444

--- baek8111(0과1).cpp
+++ baek8111(0과1).cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-#include <memory.h>
-#include <queue>
-#include <string>
-using namespace std;
-bool visit[20001];
-typedef struct {
-	int m;
-	string s;
-} is;
-queue<is> q;
-int num;
-string bfs() {
-	if (q.empty()) return "";
-	int cur = q.front().m;
-	string path = q.front().s;
-	if (cur == 0)
-		return path;
-	q.pop();
-
-	if (!visit[(cur * 10) % num]) {
-		visit[(cur * 10) % num] = 1;
-		q.push({ (cur * 10) % num, path + "0" });
-	}
-	
-	if (!visit[((cur * 10) + 1) % num]) {
-		visit[((cur * 10) + 1) % num] = 1;
-		q.push({ ((cur * 10)+1) % num, path + "1" });
-	}
-
-	return bfs();
-}
-int main() {
-	int t; cin >> t;
-	for (int i = 0; i < t; i++) {
-		cin >> num;
-		q.push({ 1,"1" });
-		visit[1] = 1;
-		cout<<bfs()<<endl;
-		while (!q.empty())q.pop();
-		memset(visit, 0, sizeof(visit));
-	}
-	return 0;
-}
+//#include <iostream>
+//#include <memory.h>
+//#include <queue>
+//#include <string>
+//using namespace std;
+//bool visit[20001];
+//typedef struct {
+//	int m;
+//	string s;
+//} is;
+//queue<is> q;
+//int num;
+//string bfs() {
+//	if (q.empty()) return "";
+//	int cur = q.front().m;
+//	string path = q.front().s;
+//	if (cur == 0)
+//		return path;
+//	q.pop();
+//
+//	if (!visit[(cur * 10) % num]) {
+//		visit[(cur * 10) % num] = 1;
+//		q.push({ (cur * 10) % num, path + "0" });
+//	}
+//	
+//	if (!visit[((cur * 10) + 1) % num]) {
+//		visit[((cur * 10) + 1) % num] = 1;
+//		q.push({ ((cur * 10)+1) % num, path + "1" });
+//	}
+//
+//	return bfs();
+//}
+//int main() {
+//	int t; cin >> t;
+//	for (int i = 0; i < t; i++) {
+//		cin >> num;
+//		q.push({ 1,"1" });
+//		visit[1] = 1;
+//		cout<<bfs()<<endl;
+//		while (!q.empty())q.pop();
+//		memset(visit, 0, sizeof(visit));
+//	}
+//	return 0;
+//}
