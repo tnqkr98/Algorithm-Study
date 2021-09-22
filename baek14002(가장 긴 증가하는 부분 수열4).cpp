9077b84509d1ab80f0b15e5d5111456ccba20df2
donghan <tnqkr98@naver.com>
2021-09-22 23:53:34 +0900

    baek14002

---
+++ baek14002(가장 긴 증가하는 부분 수열4).cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+using namespace std;
+vector<int> darr;
+int arr[1001], d[1001];
+int find(int num) {
+	int l = 0, r = darr.size()-1;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+		if (darr[mid] > num) r = mid - 1;
+		else if (darr[mid] == num) return mid-1;
+		else l = mid + 1;
+	}
+	return l-1;
+}
+int main() {
+	int n; cin >> n;
+	int maxidx = 0;
+	stack<int> s;
+	darr.push_back(0);
+	for (int i = 1; i <= n; i++){
+		cin >> arr[i];
+		int lis = find(arr[i]);
+		d[i] = lis + 1;
+		if (darr.size() - 1 < lis + 1) {
+			darr.push_back(arr[i]);
+			maxidx = i;
+		}
+		else 
+			darr[lis + 1] = min(darr[lis+1], arr[i]);
+	}
+	cout << darr.size() - 1<<endl;
+	int lis = d[maxidx];
+	int val = arr[maxidx];
+	s.push(val);
+	for (int i = maxidx-1; i >= 1; i--) 
+		if (d[i] == lis - 1 && arr[i] < val) {
+			s.push(arr[i]);
+			val = arr[i];
+			lis = d[i];
+		}
+	while (!s.empty()) {
+		cout << s.top() << " ";
+		s.pop();
+	}
+	return 0;
+}
