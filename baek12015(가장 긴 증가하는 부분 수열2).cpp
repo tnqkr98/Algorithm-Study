a5f6c60bac2102a6dc8d20aca087a6517ccc94e5
donghan <tnqkr98@naver.com>
2021-09-22 23:56:17 +0900

    baek12015

---
+++ baek12015(가장 긴 증가하는 부분 수열2).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<int> darr;
+int find(int num) {
+	int l = 0, r = darr.size() - 1;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+		if (darr[mid] > num) r = mid - 1;
+		else if (darr[mid] == num) return mid - 1;
+		else l = mid + 1;
+	}
+	return l - 1;
+}
+int main() {
+	int n; cin >> n;
+	darr.push_back(0);
+	for (int i = 1; i <= n; i++) {
+		int t; cin >> t;
+		int lis = find(t);
+		if (darr.size() - 1 < lis + 1)
+			darr.push_back(t);
+		else
+			darr[lis + 1] = min(darr[lis + 1], t);
+	}
+	cout << darr.size() - 1;
+	return 0;
+}
