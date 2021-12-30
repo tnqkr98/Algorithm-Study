7a8b1ff79749c1cd75a4b291e2bb27cb5c55e306
donghan <tnqkr98@naver.com>
2021-12-30 22:51:57 +0900

    baek20352

---
+++ BeakjoonOnlineJudge/baek20352(Circus).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+int main() {
+   double n; cin >> n;
+   cout.precision(12);
+   cout << 2 * sqrt(n / M_PI)*M_PI;
+   return 0;
+}
