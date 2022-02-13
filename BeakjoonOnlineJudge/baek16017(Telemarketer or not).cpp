5adf99bc89c5291b7ff1f7d3bc109120cef97a4e
unknown <tnqkr98@naver.com>
2022-02-14 00:16:55 +0900

    baek16017

---
+++ BeakjoonOnlineJudge/baek16017(Telemarketer or not).cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a, b, c, d; cin >> a >> b >> c >> d;
+//	if (a >= 8 && b == c && d >= 8) cout << "ignore";
+//	else cout << "answer";
+//
+//
+//	return 0;
+//}
