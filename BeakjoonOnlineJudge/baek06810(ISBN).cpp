e2eab541f9b42511e494dfbb453584ed341f29be
donghan <tnqkr98@naver.com>
2022-03-16 14:33:05 +0900

    baek6810

---
+++ BeakjoonOnlineJudge/baek06810(ISBN).cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int sum = 91;
+	int a, b, c; cin >> a >> b >> c;
+	cout << "The 1-3-sum is " << sum + a * 1 + b * 3 + c * 1;
+
+	return 0;
+}
