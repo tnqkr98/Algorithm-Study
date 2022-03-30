6bcc7bb9da846dd9c9450dd44f707f2b8aee7770
donghan <tnqkr98@naver.com>
2022-03-30 19:01:31 +0900

    baek10599

---
+++ BeakjoonOnlineJudge/baek10599(페르시아의 왕들).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	while (1) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		if (a == 0 && b == 0 && c == 0 && d == 0) break;
+		cout << c - b << " " << d - a << endl;
+	}
+	return 0;
+}
