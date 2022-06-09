1a7846a260f0f1ed0453afc46d7e3f104a7440a9
donghan <tnqkr98@naver.com>
2022-06-09 22:19:19 +0900

    Baek4999

---
+++ BeakjoonOnlineJudge/baek04999(아!).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s1, s2;
+	cin >> s1 >> s2;
+	if (s1.size() >= s2.size()) cout << "go";
+	else cout << "no";
+
+	return 0;
+}
