b5768aec716db9229d243e476677a13b1b26fe8c
donghan <tnqkr98@naver.com>
2022-06-11 23:15:51 +0900

    baek9086

---
+++ BeakjoonOnlineJudge/baek09086(문자열).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		string s; cin >> s;
+		cout << s[0] << s[s.size() - 1] << endl;
+	}
+	return 0;
+}
