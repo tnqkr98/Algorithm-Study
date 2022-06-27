62af4cc2f2f82b489859fa6f18ac5f315ed632c5
donghan <tnqkr98@naver.com>
2022-06-27 23:46:48 +0900

    baek25304

---
+++ BeakjoonOnlineJudge/baek25304(영수증).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x, n; cin >> x >> n;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		int a, b; cin >> a >> b;
+		sum += a * b;
+	}
+	if (x != sum)cout << "No";
+	else cout << "Yes";
+
+	return 0;
+}
