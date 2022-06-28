51fd7a11fec423d8ab81ebd8caaf4e27fff1d71b
donghan <tnqkr98@naver.com>
2022-06-28 22:15:42 +0900

    baek2566

--- BeakjoonOnlineJudge/baek02566(최댓값).cpp
+++ BeakjoonOnlineJudge/baek02566(최댓값).cpp
@@ -2,15 +2,14 @@
 using namespace std;
 int main() {
 	int max = 0, r, c;
-	for (int i = 0; i < 9; i++)
+	for(int i=0;i<9;i++)
 		for (int j = 0; j < 9; j++) {
 			int t; cin >> t;
-			if (t > max) {
+			if (t >= max) {
 				max = t;
-				r = i;
-				c = j;
+				r = i+1; c = j+1;
 			}
 		}
-	cout << max << endl << r+1 << " " << c+1;
+	cout << max << endl << r << " " << c;
 	return 0;
 }
