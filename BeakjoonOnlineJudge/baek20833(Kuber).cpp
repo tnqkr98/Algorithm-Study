0998f4d29e50f474f7d05edb497faaf882ae0396
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2022-07-06 22:52:24 +0900

    baek20833

---
+++ BeakjoonOnlineJudge/baek20833(Kuber).cpp
@@ -0,0 +1,9 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int n; cin >> n;
+//	long long sum = 0;
+//	for (int i = 1; i <= n; i++) sum += i * i * i;
+//	cout << sum;
+//	return 0;
+//}
