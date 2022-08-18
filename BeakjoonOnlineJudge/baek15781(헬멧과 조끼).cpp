ddd15e2f9efac8c5a752eaea4ecddb680d2facb2
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2022-08-19 01:10:20 +0900

    baek15781

---
+++ BeakjoonOnlineJudge/baek15781(헬멧과 조끼).cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n, m; cin >> n >> m;
+//	int mn=0, mm=0;
+//	for (int i = 0; i < n; i++) {
+//		int a; cin >> a;
+//		if (a > mn) mn = a;
+//	}
+//	for (int i = 0; i < m; i++) {
+//		int a; cin >> a;
+//		if (a > mm) mm = a;
+//	}
+//	cout << mn + mm;
+//	return 0;
+//}
