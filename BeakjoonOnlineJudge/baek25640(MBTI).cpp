f73bd1d98b63a90c56d2c6eac5b4425491bb7f73
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2022-09-28 01:23:30 +0900

    baek25640

---
+++ BeakjoonOnlineJudge/baek25640(MBTI).cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+//	string s; cin >> s;
+//	int n,c=0; cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		string t; cin >> t;
+//		if (s == t)c++;
+//	}
+//	cout << c;
+//	return 0;
+//}
