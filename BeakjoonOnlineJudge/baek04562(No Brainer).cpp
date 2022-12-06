507dcf9d8bf0cac83c25ddf82e17b30c501afb8e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2022-12-07 00:08:37 +0900

    baek4562

---
+++ BeakjoonOnlineJudge/baek04562(No Brainer).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b; cin >> a >> b;
+		if (a < b) cout << "NO BRAINS" << endl;
+		else cout << "MMM BRAINS" << endl;
+	}
+	return 0;
+}
