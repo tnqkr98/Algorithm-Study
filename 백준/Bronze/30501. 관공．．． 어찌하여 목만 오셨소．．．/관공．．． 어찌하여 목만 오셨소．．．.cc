55f35f0f0f62a0a396a6c432ae0a0c9384351930
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2023-11-29 00:36:50 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		if (s.find('S') != -1) cout << s;
+	}
+	return 0;
+}
