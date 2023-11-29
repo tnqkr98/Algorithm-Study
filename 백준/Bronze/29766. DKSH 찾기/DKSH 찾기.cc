ce5a560be56027e1a38eef95f2aa0fdb6e1af7e0
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2023-11-29 21:54:06 +0900

    [Bronze IV] Title: DKSH 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n = 0,c =0;
+	string s;
+	cin >> s;
+	while (1) {
+		n = s.find("DKSH", n);
+		if (n != -1) {
+			c++;
+			n += 1;
+		}
+		else break;
+	}
+	cout << c;
+	return 0;
+}
