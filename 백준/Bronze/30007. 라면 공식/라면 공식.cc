f5fbe974a2af5afe75ef1b01e90b66826faf22f5
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2023-12-11 00:51:31 +0900

    [Bronze V] Title: 라면 공식, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b, x; cin >> a >> b >> x;
+		cout << a * (x - 1) + b << endl;
+	}
+	return 0;
+}
