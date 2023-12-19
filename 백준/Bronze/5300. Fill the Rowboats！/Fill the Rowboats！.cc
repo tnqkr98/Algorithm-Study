c64c733ff7cfa80f4c52cb87832872f032779e08
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2023-12-19 23:09:04 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << i << " ";
+		if (i % 6 == 0) cout << "Go!" << " ";
+	}
+	if (n % 6 != 0) cout << "Go!";
+	return 0;
+}
