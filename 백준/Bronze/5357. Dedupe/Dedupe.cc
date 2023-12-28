4e7d1f11089752157d581694cbfb236ab4998dec
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2023-12-28 22:13:45 +0900

    [Bronze IV] Title: Dedupe, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5357. Dedupe/Dedupe.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		char last = ' ';
+		for (int j = 0; j < s.length(); j++) {
+			if (last != s[j]) {
+				cout << s[j];
+				last = s[j];
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
