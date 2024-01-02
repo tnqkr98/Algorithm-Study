9e61533e953370cef3e26429fee4dfca2fda9c8b
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-01-02 23:05:30 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 368 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int y; cin >> y;
+		int r = y % 100;
+		if ((y+1) % r == 0) cout << "Good"<<endl;
+		else cout << "Bye"<<endl;
+	}
+
+
+	return 0;
+}
