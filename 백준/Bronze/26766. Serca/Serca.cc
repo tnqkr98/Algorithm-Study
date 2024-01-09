3af49e2c5a160c3f916b38a3926ce8899acfa7bb
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-01-09 23:06:57 +0900

    [Bronze V] Title: Serca, Time: 136 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main() {
+	int n; scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		printf(" @@@   @@@ \n");
+		printf("@   @ @   @\n");
+		printf("@    @    @\n");
+		printf("@         @\n");
+		printf(" @       @ \n");
+		printf("  @     @  \n");
+		printf("   @   @   \n");
+		printf("    @ @    \n");
+		printf("     @     \n");
+	}
+	return 0;
+}
