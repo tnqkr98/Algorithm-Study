8f5575b059a0755b65bc373a3846bcb28c6f618b
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-01-21 04:33:34 +0900

    [Bronze IV] Title: Buying in Bulk, Time: 648 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26332. Buying in Bulk/Buying in Bulk.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b; cin >> a >> b;
+		cout << a << " " << b << endl;
+		cout << b + (b - 2) * (a - 1) << endl;
+	}
+
+	return 0;
+}
