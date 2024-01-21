e6def751f950221004e87c6916433d8010177998
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-01-22 01:48:10 +0900

    [Bronze IV] Title: Periods, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26560. Periods/Periods.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int n; cin >> n; cin.ignore();
+	for (int i = 0; i < n; i++) {
+		string s; 
+		getline(cin, s);
+		if (s[s.length() - 1] != '.')cout << s << '.' << endl;
+		else cout << s << endl;
+	}
+
+	return 0;
+}
