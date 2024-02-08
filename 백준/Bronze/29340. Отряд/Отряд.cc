773f5d190d35beab2d8919aaadecf8be5b8f1196
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-02-09 01:58:47 +0900

    [Bronze IV] Title: Отряд, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29340. Отряд/Отряд.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string a, b; cin >> a >> b;
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] >= b[i]) cout << a[i];
+		else cout << b[i];
+	}
+
+
+	return 0;
+}
