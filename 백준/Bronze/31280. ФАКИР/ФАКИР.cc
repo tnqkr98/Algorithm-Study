70d3158f860207a1abca611b69f239ace2a6b09c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-02-20 23:04:25 +0900

    [Bronze IV] Title: ФАКИР, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31280. ФАКИР/ФАКИР.cc
+++ 백준/Bronze/31280. ФАКИР/ФАКИР.cc
@@ -5,18 +5,15 @@ using namespace std;
 int main() {
 	int a, b, c, d; cin >> a >> b >> c >> d;
 	vector<int> v;
-
 	v.push_back(a);
 	v.push_back(b);
 	v.push_back(c);
 	v.push_back(d);
 	sort(v.begin(), v.end());
-	 
 	int sum = 0;
 	for (int i = v.size() - 1; i > 0; i--) {
 		sum += v[i];
 	}
-
 	cout << sum + 1;
 	return 0;
 }
