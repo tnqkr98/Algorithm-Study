5eba4ef2c59aaa21deef35919546aa0d067dc791
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-03-02 01:41:49 +0900

    [Bronze IV] Title: Os últimos serão os primeiros, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30658. Os últimos serão os primeiros/Os últimos serão os primeiros.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	while (1) {
+		int n; cin >> n;
+		vector<int> v;
+		if (n == 0) break;
+		for (int i = 0; i < n; i++) {
+			int a; cin >> a;
+			v.push_back(a);
+		}
+		for (int i = 0; i < n; i++) {
+			cout << v[n - i-1]<<endl;
+		}
+		cout << '0' << endl;
+	}
+
+	return 0;
+}
