ad325ba93f535ecdca2f4150a852762a1fc9aca3
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-03-04 22:26:27 +0900

    [Bronze III] Title: 알파벳 뒤집기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/30032. 알파벳 뒤집기/알파벳 뒤집기.cc
+++ 백준/Bronze/30032. 알파벳 뒤집기/알파벳 뒤집기.cc
@@ -7,12 +7,10 @@ int main() {
 	m[1]['b'] = 'p';
 	m[1]['q'] = 'd';
 	m[1]['p'] = 'b';
-
 	m[2]['d'] = 'b';
 	m[2]['b'] = 'd';
 	m[2]['q'] = 'p';
 	m[2]['p'] = 'q';
-
 	int n,d; cin >> n>>d;
 	for (int i = 0; i < n; i++) {
 		string s; cin >> s;
@@ -21,6 +19,5 @@ int main() {
 		}
 		cout << endl;
 	}
-	
 	return 0;
 }
