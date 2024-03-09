31fc8ec78607146a6044ad31c2e80f6661415184
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-03-09 16:11:34 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	long long c = 0;
+	int n; cin >> n;
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = i + 1; j <= n; j++) {
+			c++;
+		}
+	}
+	cout << c << endl<<2;
+
+
+	return 0;
+}
