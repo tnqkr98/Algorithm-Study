fce45782c57ac6c096c02e7680480109e2e03efe
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-03-12 22:29:49 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 184 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val arr = Array(6) { nextInt() }
+    for(x in -999 .. 999){
+        for (y in -999 .. 999){
+            if(arr[0]*x + arr[1]*y == arr[2] && arr[3]*x + arr[4]*y == arr[5]) {
+                print("$x $y")
+            }
+        }
+    }
+}
