3affad4b13768cdad007e0c22670e3872abef87f
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-03-24 22:52:12 +0900

    [Silver III] Title: 분수 합, Time: 124 ms, Memory: 13544 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.kt
+++ 백준/Silver/1735. 분수 합/분수 합.kt
@@ -4,7 +4,7 @@ fun gcd(a: Int, b: Int): Int {
     var big = if (a > b) a else b
     var small = if (a > b) b else a
     while (big % small != 0) {
-       val tmp = big
+        val tmp = big
         big = small
         small = tmp % small
     }
@@ -17,9 +17,9 @@ fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val c2 = nextInt()
     val p2 = nextInt()
 
-    val lcm = p1 * p2 / gcd(p1,p2)
+    val lcm = p1 * p2 / gcd(p1, p2)
     val nc1 = (lcm / p1) * c1
     val nc2 = (lcm / p2) * c2
     val nc = nc1 + nc2
-    print("${nc/gcd(nc,lcm)} ${lcm/gcd(nc,lcm)}")
+    print("${nc / gcd(nc, lcm)} ${lcm / gcd(nc, lcm)}")
 }
