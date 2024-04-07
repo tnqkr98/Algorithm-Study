4814a6e4acfc185eaf802563bfa401162e40bf6e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-04-07 23:46:36 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	if (n >= 2) {
+		cout << ((n - 2) % 7 == 0) ? 1 : 0;
+	} else {
+		if (n == 1) cout << 0;
+		else cout << 1;
+	}
+
+	return 0;
+}
