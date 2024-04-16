7097275e0b48eba1513b591588e02c2325384490
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-04-17 00:49:17 +0900

    [Bronze V] Title: Loteria Falha, Time: 144 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/30664. Loteria Falha/Loteria Falha.kt
+++ 백준/Bronze/30664. Loteria Falha/Loteria Falha.kt
@@ -2,11 +2,11 @@ import java.math.BigInteger
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
-    while(true) {
+    while (true) {
         val s = next()
         val n = BigInteger(s)
-        if(n == 0.toBigInteger()) return@with
-        if(n%42.toBigInteger() == 0.toBigInteger()) println("PREMIADO")
+        if (n == 0.toBigInteger()) return@with
+        if (n % 42.toBigInteger() == 0.toBigInteger()) println("PREMIADO")
         else println("TENTE NOVAMENTE")
     }
 }
