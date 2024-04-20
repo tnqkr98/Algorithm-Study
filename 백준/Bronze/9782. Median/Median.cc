aacd17b83302a3453b213fc4e40372b244b3bccd
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-04-21 00:54:28 +0900

    [Bronze IV] Title: Median, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9782. Median/Median.cc
+++ 백준/Bronze/9782. Median/Median.cc
@@ -5,21 +5,14 @@ int main() {
 	int idx = 1;
 	while (1) {
 		int n; cin >> n;
-
 		if (n == 0) break;
 		vector<double> v;
 		for (int i = 0; i < n; i++) {
 			double a; cin >> a;
 			v.push_back(a);
 		}
-		
-		if (n % 2 == 1) {
-			printf("Case %d: %.1f\n",idx++, v[n/2]);
-		}
-		else {
-			printf("Case %d: %.1f\n", idx++, (v[n/2] + v[n/2-1])/2);
-		}
+		if (n % 2 == 1) printf("Case %d: %.1f\n",idx++, v[n/2]);
+		else printf("Case %d: %.1f\n", idx++, (v[n/2] + v[n/2-1])/2);
 	}
-
 	return 0;
 }
