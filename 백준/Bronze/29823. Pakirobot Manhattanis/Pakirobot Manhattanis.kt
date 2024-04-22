50b3e25a34742d607eec351d6af33320eb74b8cc
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-04-22 22:40:11 +0900

    [Bronze IV] Title: Pakirobot Manhattanis, Time: 128 ms, Memory: 13552 KB -BaekjoonHub

--- 백준/Bronze/29823. Pakirobot Manhattanis/Pakirobot Manhattanis.kt
+++ 백준/Bronze/29823. Pakirobot Manhattanis/Pakirobot Manhattanis.kt
@@ -4,9 +4,9 @@ import kotlin.math.abs
 fun main() = with(Scanner(System.`in`)) {
     nextInt()
     val str = next()
-    val n = str.count { it == 'N'}
-    val s = str.count { it == 'S'}
-    val w = str.count { it == 'W'}
-    val e = str.count { it == 'E'}
-    print(abs(n-s)+abs(w-e))
+    val n = str.count { it == 'N' }
+    val s = str.count { it == 'S' }
+    val w = str.count { it == 'W' }
+    val e = str.count { it == 'E' }
+    print(abs(n - s) + abs(w - e))
 }
