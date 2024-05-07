1dbbb8f6a1f3a4cd04203fd847d75c53fa33c256
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-07 23:56:14 +0900

    [Bronze IV] Title: gahui and sousenkyo 3, Time: 128 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Bronze/30793. gahui and sousenkyo 3/gahui and sousenkyo 3.kt
+++ 백준/Bronze/30793. gahui and sousenkyo 3/gahui and sousenkyo 3.kt
@@ -3,8 +3,8 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val p = nextDouble()
     val x = nextDouble()
-    val v = p/x
-    if(v<0.2) print("weak")
+    val v = p / x
+    if (v < 0.2) print("weak")
     else if (v >= 0.2 && v < 0.4) print("normal")
     else if (v >= 0.4 && v < 0.6) print("strong")
     else if (v >= 0.6) print("very strong")
