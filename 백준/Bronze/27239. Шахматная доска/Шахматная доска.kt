42b65871ef7756ea751640485a97c045746a5299
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-11 02:39:19 +0900

    [Bronze IV] Title: Шахматная доска, Time: 116 ms, Memory: 13312 KB -BaekjoonHub

---
+++ 백준/Bronze/27239. Шахматная доска/Шахматная доска.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val arr = Array(8) {
+        Array(8) { 0 }
+    }
+
+    val map = mutableMapOf<Int, String>()
+    var cnt = 1
+
+    for(i in 1 .. 8) {
+        for (j in 0 .. 7) {
+            map[cnt++] = "${'a'+j}$i"
+        }
+    }
+    val n = nextInt()
+    print(map[n])
+}
