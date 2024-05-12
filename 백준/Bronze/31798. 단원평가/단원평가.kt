4bdb1a5e07554ce8d4ba2c546530f4c1cec9ee9d
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-12 23:15:00 +0900

    [Bronze III] Title: 단원평가, Time: 120 ms, Memory: 13244 KB -BaekjoonHub

--- 백준/Bronze/31798. 단원평가/단원평가.kt
+++ 백준/Bronze/31798. 단원평가/단원평가.kt
@@ -5,13 +5,13 @@ fun main() = with(Scanner(System.`in`)) {
     val a = nextInt()
     val b = nextInt()
     val c = nextInt()
-    var res:Int = 0
-    res = if(a == 0) {
-        c*c - b
-    } else if(b == 0) {
-        c*c - a
+    var res: Int = 0
+    res = if (a == 0) {
+        c * c - b
+    } else if (b == 0) {
+        c * c - a
     } else {
-        sqrt(a.toDouble()+b.toDouble()).toInt()
+        sqrt(a.toDouble() + b.toDouble()).toInt()
     }
     print(res)
 }
