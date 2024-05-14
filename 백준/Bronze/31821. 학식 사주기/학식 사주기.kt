07b6e0ee13ed4e88fef6e81b3fdc1c1519228052
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-14 15:55:58 +0900

    [Bronze IV] Title: 학식 사주기, Time: 120 ms, Memory: 13264 KB -BaekjoonHub

---
+++ 백준/Bronze/31821. 학식 사주기/학식 사주기.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val a = Array(n) {
+        nextInt()
+    }
+    val m = nextInt()
+    var sum = 0
+    for(i in 0 until m) {
+        val t = nextInt()
+        sum += a[t-1]
+    }
+    print(sum)
+}
