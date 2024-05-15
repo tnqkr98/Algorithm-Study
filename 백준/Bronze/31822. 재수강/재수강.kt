dcd87fbaf5a9379d330d124464a352460cf58993
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-15 23:40:31 +0900

    [Bronze IV] Title: 재수강, Time: 152 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/31822. 재수강/재수강.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val s = next()
+    val ss = s.substring(0..4)
+    val n = nextInt()
+    var c = 0
+    for(i in 0 until n) {
+        val a = next()
+        val aa = a.substring(0..4)
+        if(aa == ss) c++
+    }
+    print(c)
+}
