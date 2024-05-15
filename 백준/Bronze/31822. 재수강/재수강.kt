42463867779c607220a932572ae83124e3f9c8e7
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-15 23:41:13 +0900

    [Bronze IV] Title: 재수강, Time: 156 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Bronze/31822. 재수강/재수강.kt
+++ 백준/Bronze/31822. 재수강/재수강.kt
@@ -5,10 +5,10 @@ fun main() = with(Scanner(System.`in`)) {
     val ss = s.substring(0..4)
     val n = nextInt()
     var c = 0
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val a = next()
         val aa = a.substring(0..4)
-        if(aa == ss) c++
+        if (aa == ss) c++
     }
     print(c)
 }
