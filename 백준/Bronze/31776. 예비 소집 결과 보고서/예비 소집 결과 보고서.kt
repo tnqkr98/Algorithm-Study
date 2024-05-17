b08deac85a39728cd1741b9521614402d3d9dcc2
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-18 00:04:14 +0900

    [Bronze III] Title: 예비 소집 결과 보고서, Time: 140 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/31776. 예비 소집 결과 보고서/예비 소집 결과 보고서.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var c = 0
+    for (i in 0 until n) {
+        val a1 = nextInt()
+        val a2 = nextInt()
+        val a3 = nextInt()
+
+        if (a1 == -1) continue;
+        if (a2 != -1 && a1 > a2) continue;
+        if (a2 == -1 && a3 != -1)  continue;
+        if (a3 != -1 && a2 > a3) continue;
+        c++
+    }
+    print(c)
+}
