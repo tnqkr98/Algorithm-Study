60e0b428b2ef04b832898b0abe08b3ec4508335f
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-19 18:58:46 +0900

    [Bronze III] Title: Matches, Time: 140 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Bronze/13222. Matches/Matches.kt
+++ 백준/Bronze/13222. Matches/Matches.kt
@@ -5,10 +5,10 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     val w = nextDouble()
     val h = nextDouble()
-    val l = sqrt(w*w + h*h)
-    for(i in 0 until n) {
+    val l = sqrt(w * w + h * h)
+    for (i in 0 until n) {
         val a = nextInt()
-        if(a<=l) println("YES")
+        if (a <= l) println("YES")
         else println("NO")
     }
 }
