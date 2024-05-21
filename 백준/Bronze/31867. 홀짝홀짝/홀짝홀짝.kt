15eeb5682cbc7d0449c35347154af8eb91ce8587
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-21 22:24:22 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 356 ms, Memory: 25076 KB -BaekjoonHub

---
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val s = next()
+
+    var e = 0
+    var o = 0
+    s.forEach {
+        if(it.digitToInt() % 2 == 0) {
+            e++
+        } else {
+            o++
+        }
+    }
+    if(e>o) print(0)
+    else if(o>e) print(1)
+    else print(-1)
+}
