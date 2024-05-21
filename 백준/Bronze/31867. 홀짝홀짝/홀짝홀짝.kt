0bcd591299cccf161d7ff069ab13015e0dd2825a
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-21 22:26:28 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 384 ms, Memory: 25068 KB -BaekjoonHub

--- 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.kt
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.kt
@@ -7,13 +7,13 @@ fun main() = with(Scanner(System.`in`)) {
     var e = 0
     var o = 0
     s.forEach {
-        if(it.digitToInt() % 2 == 0) {
+        if (it.digitToInt() % 2 == 0) {
             e++
         } else {
             o++
         }
     }
-    if(e>o) print(0)
-    else if(o>e) print(1)
+    if (e > o) print(0)
+    else if (o > e) print(1)
     else print(-1)
 }
