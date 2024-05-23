ed902ff6aa356077d24964193eb5b9aa599a1316
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-23 22:58:36 +0900

    [Bronze III] Title: 수박 게임, Time: 116 ms, Memory: 13268 KB -BaekjoonHub

--- 백준/Bronze/31868. 수박 게임/수박 게임.kt
+++ 백준/Bronze/31868. 수박 게임/수박 게임.kt
@@ -3,8 +3,8 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var k = nextLong()
-    for(i in 0 until n-1) {
-        k/=2
+    for (i in 0 until n - 1) {
+        k /= 2
     }
     print(k)
 }
