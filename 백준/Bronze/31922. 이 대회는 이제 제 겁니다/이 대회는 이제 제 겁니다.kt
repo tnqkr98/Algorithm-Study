8282684565c42b549d9fd8ec07ebcfd58132026c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-27 23:07:17 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 120 ms, Memory: 13232 KB -BaekjoonHub

--- 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.kt
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.kt
@@ -4,6 +4,6 @@ fun main() = with(Scanner(System.`in`)) {
     val a = nextInt()
     val b = nextInt()
     val p = nextInt()
-    if(a+p>b) print(a+p)
+    if (a + p > b) print(a + p)
     else print(b)
 }
