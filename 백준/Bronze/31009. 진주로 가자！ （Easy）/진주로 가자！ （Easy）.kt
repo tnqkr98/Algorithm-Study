89a7885629f6a262f17eba715079a371734d9d75
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-29 22:49:43 +0900

    [Bronze III] Title: 진주로 가자! (Easy), Time: 196 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/31009. 진주로 가자！ （Easy）/진주로 가자！ （Easy）.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val m = mutableMapOf<String, Int>()
+    for (i in 0 until n) {
+        val s = next()
+        val c = nextInt()
+        m[s] = c
+    }
+    val jin = m["jinju"]!!
+    var c = 0
+    m.forEach {
+        if(it.value >  jin) c++
+    }
+    println(m["jinju"])
+    print(c)
+}
