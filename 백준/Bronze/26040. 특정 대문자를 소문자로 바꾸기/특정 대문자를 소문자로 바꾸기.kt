c4e5c72f7cdc8b7173743f1defdaa7d2f0572338
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-05-31 23:17:06 +0900

    [Bronze III] Title: 특정 대문자를 소문자로 바꾸기, Time: 244 ms, Memory: 22016 KB -BaekjoonHub

---
+++ 백준/Bronze/26040. 특정 대문자를 소문자로 바꾸기/특정 대문자를 소문자로 바꾸기.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var s = nextLine()
+    val c = nextLine()
+
+    val dif = 'A' - 'a'
+    c.split(" ").forEach {
+        s = s.replace(it[0],(it[0].code - dif).toChar())
+    }
+    print(s)
+}
