a5fe25fa178c379e57d655b6a36349e35b0f9c07
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-02 23:41:53 +0900

    [Bronze III] Title: Mõttemeister, Time: 700 ms, Memory: 28344 KB -BaekjoonHub

---
+++ 백준/Bronze/7181. Mõttemeister/Mõttemeister.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val s = next()
+    val set = s.toList().toSet()
+    val n = nextInt()
+    for (i in 0 until n) {
+        val a = next()
+        var cor = 0
+        var hav = 0
+        a.forEachIndexed { index, it->
+            if(s[index] == it) cor ++
+            if(set.contains(it)) hav ++
+        }
+        println("$hav $cor")
+    }
+}
