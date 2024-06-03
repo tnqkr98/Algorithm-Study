66fd0ddf962541009d9169fe3053b70a6401b7e2
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-04 03:45:26 +0900

    [Bronze IV] Title: 브실이와 친구가 되고 싶어 🤸‍♀️, Time: 120 ms, Memory: 13264 KB -BaekjoonHub

--- 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.kt
+++ 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.kt
@@ -6,11 +6,11 @@ fun main() = with(Scanner(System.`in`)) {
     val k = nextInt()
     val x = nextInt()
     var c = 0
-    val minx = if(k-x <0) 0 else k-x
-    val maxx = k+x
-    for(i in minx until maxx+1) {
-        if(i >= a && i <= b) c++
+    val minx = if (k - x < 0) 0 else k - x
+    val maxx = k + x
+    for (i in minx until maxx + 1) {
+        if (i >= a && i <= b) c++
     }
-    if(c != 0) print(c)
+    if (c != 0) print(c)
     else print("IMPOSSIBLE")
 }
