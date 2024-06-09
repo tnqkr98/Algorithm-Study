7de3afa797c114f6c7fd659fe5d29dabed82bac3
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-09 19:19:23 +0900

    [Bronze III] Title: ABCD-код, Time: 540 ms, Memory: 25820 KB -BaekjoonHub

--- 백준/Bronze/19774. ABCD－код/ABCD－код.kt
+++ 백준/Bronze/19774. ABCD－код/ABCD－код.kt
@@ -4,10 +4,10 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     for (i in 0 until n) {
         val a = nextInt()
-        val x = a/100
-        val y = a%100
-        val res = (x*x + y*y)%7
-        if(res == 1) {
+        val x = a / 100
+        val y = a % 100
+        val res = (x * x + y * y) % 7
+        if (res == 1) {
             println("YES")
         } else {
             println("NO")
