8026a5c787f98e5b2e5ba99eb79c7ac62ef715b4
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-09 19:16:27 +0900

    [Bronze III] Title: ABCD-код, Time: 424 ms, Memory: 27380 KB -BaekjoonHub

---
+++ 백준/Bronze/19774. ABCD－код/ABCD－код.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    for (i in 0 until n) {
+        val a = nextInt()
+        val x = a/100
+        val y = a%100
+        val res = (x*x + y*y)%7
+        if(res == 1) {
+            println("YES")
+        } else {
+            println("NO")
+        }
+    }
+}
