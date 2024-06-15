3cdd468f2be5d8cd93a444c37e9f498e9ad50478
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-15 18:58:39 +0900

    [Bronze IV] Title: 특별한 작은 분수, Time: 120 ms, Memory: 13392 KB -BaekjoonHub

---
+++ 백준/Bronze/27890. 특별한 작은 분수/특별한 작은 분수.kt
@@ -0,0 +1,16 @@
+import java.util.*
+import kotlin.math.floor
+
+fun main() = with(Scanner(System.`in`)) {
+    var x = nextInt()
+    val n = nextInt()
+
+    for (i in 1 .. n) {
+        x = if(x % 2 == 0) {
+            floor(x.toDouble()/2).toInt() xor 6
+        } else {
+            (x*2) xor 6
+        }
+    }
+    print(x)
+}
