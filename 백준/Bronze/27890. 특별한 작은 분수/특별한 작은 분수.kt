d164e1eb6f0b05ddb51610045524923e30b66d0c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-15 18:59:11 +0900

    [Bronze IV] Title: 특별한 작은 분수, Time: 120 ms, Memory: 13340 KB -BaekjoonHub

--- 백준/Bronze/27890. 특별한 작은 분수/특별한 작은 분수.kt
+++ 백준/Bronze/27890. 특별한 작은 분수/특별한 작은 분수.kt
@@ -5,11 +5,11 @@ fun main() = with(Scanner(System.`in`)) {
     var x = nextInt()
     val n = nextInt()
 
-    for (i in 1 .. n) {
-        x = if(x % 2 == 0) {
-            floor(x.toDouble()/2).toInt() xor 6
+    for (i in 1..n) {
+        x = if (x % 2 == 0) {
+            floor(x.toDouble() / 2).toInt() xor 6
         } else {
-            (x*2) xor 6
+            (x * 2) xor 6
         }
     }
     print(x)
