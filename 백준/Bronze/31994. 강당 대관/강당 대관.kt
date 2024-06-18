140f0d0fd422752e7304a53499e34cb611deb561
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-18 21:47:53 +0900

    [Bronze IV] Title: 강당 대관, Time: 124 ms, Memory: 13516 KB -BaekjoonHub

---
+++ 백준/Bronze/31994. 강당 대관/강당 대관.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var max = 0
+    var s = ""
+    for (i in 0 until 7) {
+        val ss = next()
+        val n = nextInt()
+        if(n > max) {
+            max = n
+            s = ss
+        }
+    }
+
+    print(s)
+}
