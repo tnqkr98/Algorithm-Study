e432fe306821d2bfe79352872cbc6aa4b60b5206
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-20 22:14:17 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 124 ms, Memory: 13236 KB -BaekjoonHub

--- 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.kt
+++ 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.kt
@@ -2,24 +2,22 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     var s = 0
-    for(i in 0 until 10) {
+    for (i in 0 until 10) {
         val a = nextInt()
-        if(a == 1) {
-            s+=1
-        } else if(a == 2) {
-            s+=2
-        } else {
-            s-=1
+        when (a) {
+            1 -> s += 1
+            2 -> s += 2
+            else -> s -= 1
         }
-        if(s<0){
+        if (s < 0) {
             s += 4
         }
-        s%=4
+        s %= 4
     }
-    when(s) {
-        0->print("N")
-        1->print("E")
-        2->print("S")
-        3->print("W")
+    when (s) {
+        0 -> print("N")
+        1 -> print("E")
+        2 -> print("S")
+        3 -> print("W")
     }
 }
