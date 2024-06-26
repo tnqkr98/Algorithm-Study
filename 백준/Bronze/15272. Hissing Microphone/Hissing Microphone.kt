fd8e48471c943c59d71433c3a2fd437dea9a4ba6
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-26 23:53:54 +0900

    [Bronze III] Title: Hissing Microphone, Time: 140 ms, Memory: 15392 KB -BaekjoonHub

---
+++ 백준/Bronze/15272. Hissing Microphone/Hissing Microphone.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val s = next()
+    if (s.contains("ss")) {
+        print("hiss")
+    } else {
+        print("no hiss")
+    }
+}
