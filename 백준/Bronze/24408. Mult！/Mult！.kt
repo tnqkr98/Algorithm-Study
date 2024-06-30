a2f505490a011387267f63c51e070f94acfd1afc
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-06-30 23:29:51 +0900

    [Bronze III] Title: Mult!, Time: 136 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Bronze/24408. Mult！/Mult！.kt
+++ 백준/Bronze/24408. Mult！/Mult！.kt
@@ -3,15 +3,13 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var c = 0
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val a = nextInt()
-
-        if(c == 0) {
+        if (c == 0) {
             c = a
             continue
         }
-
-        if(a % c == 0) {
+        if (a % c == 0) {
             println(a)
             c = 0
         }
