d0b7eec83504ca8102f944fd59db6e116b1fe450
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-01 22:19:09 +0900

    [Bronze III] Title: It’s Cold Here!, Time: 316 ms, Memory: 33304 KB -BaekjoonHub

---
+++ 백준/Bronze/6830. It’s Cold Here！/It’s Cold Here！.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var min = 201
+    var mc = ""
+    while(hasNext()) {
+        val c = next()
+        val n = nextInt()
+        if(n < min) {
+            min = n
+            mc = c
+        }
+    }
+    print(mc)
+}
