8a50280f5e1fcfcdb9bc49c129f70d6ccf58d6db
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-04 01:33:36 +0900

    [Bronze III] Title: Right Triangle, Time: 156 ms, Memory: 17956 KB -BaekjoonHub

--- 백준/Bronze/9723. Right Triangle/Right Triangle.kt
+++ 백준/Bronze/9723. Right Triangle/Right Triangle.kt
@@ -2,12 +2,12 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 1 .. n) {
+    for (i in 1..n) {
         val arr = Array(3) {
             nextInt()
         }
         arr.sort()
-        if(arr[0]*arr[0] + arr[1]*arr[1] == arr[2]*arr[2]) {
+        if (arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2]) {
             println("Case #$i: YES")
         } else {
             println("Case #$i: NO")
