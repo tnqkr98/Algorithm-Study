e7a02b1462fda7a84e43528e9b3984022e60d86d
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-05 23:08:06 +0900

    [Bronze III] Title: Pirates, Time: 128 ms, Memory: 13580 KB -BaekjoonHub

---
+++ 백준/Bronze/15238. Pirates/Pirates.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val s = next()
+
+    val arr = Array(26) { 0 }
+
+    s.forEach {
+        arr[it-'a']++
+    }
+    var max = 0
+    var mi = ' '
+    arr.forEachIndexed { idx, num ->
+        if(num > max) {
+            max = num
+            mi = 'a'.plus(idx)
+        }
+    }
+    print("$mi $max")
+}
