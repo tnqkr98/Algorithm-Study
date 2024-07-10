6f4824e824cda4599683df0ef779b2f705bfcc3b
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-10 21:46:42 +0900

    [Bronze III] Title: Split, Time: 104 ms, Memory: 13440 KB -BaekjoonHub

--- 백준/Bronze/21955. Split/Split.kt
+++ 백준/Bronze/21955. Split/Split.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val s = next()
-    val a = s.substring(0,s.length/2)
-    val b = s.substring(s.length/2)
+    val a = s.substring(0, s.length / 2)
+    val b = s.substring(s.length / 2)
     print("$a $b")
 }
