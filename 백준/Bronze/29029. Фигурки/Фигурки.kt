6c59b2d22d109c8594266555f63d7426d5408a52
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-12 01:07:49 +0900

    [Bronze III] Title: Фигурки, Time: 216 ms, Memory: 23812 KB -BaekjoonHub

--- 백준/Bronze/29029. Фигурки/Фигурки.kt
+++ 백준/Bronze/29029. Фигурки/Фигурки.kt
@@ -3,14 +3,14 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     val s = next()
-    val arr = Array(4) {0}
+    val arr = Array(4) { 0 }
     s.forEach {
-        when(it) {
+        when (it) {
             'N' -> arr[0]++
             'S' -> arr[1]++
             'E' -> arr[2]++
             'W' -> arr[3]++
         }
     }
-    print(n-arr.max())
+    print(n - arr.max())
 }
