726a0b2c49944ee4f000a51cb95fff721f60b1b8
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-17 22:22:02 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 120 ms, Memory: 13252 KB -BaekjoonHub

---
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val m = nextInt()
+    if(m<n) {
+        print((m*100)/2)
+    } else {
+        print((n*100)/2)
+    }
+}
