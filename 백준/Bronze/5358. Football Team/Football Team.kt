f070c99c88f6dec987fceb152231358e190b89fb
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-18 23:08:43 +0900

    [Bronze IV] Title: Football Team, Time: 120 ms, Memory: 13468 KB -BaekjoonHub

---
+++ 백준/Bronze/5358. Football Team/Football Team.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    while(hasNextLine()) {
+        val s = nextLine()
+        s.forEach {
+            if(it == 'i') print('e')
+            else if(it == 'I') print('E')
+            else if(it == 'e') print('i')
+            else if(it == 'E') print('I')
+            else print(it)
+        }
+        println()
+    }
+}
