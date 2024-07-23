3cdc5a0831e35b275005ae17499633c886ace8ba
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-23 21:43:56 +0900

    [Bronze III] Title: 거북이, Time: 136 ms, Memory: 17432 KB -BaekjoonHub

--- 백준/Bronze/2959. 거북이/거북이.kt
+++ 백준/Bronze/2959. 거북이/거북이.kt
@@ -11,7 +11,7 @@ fun main() = with(Scanner(System.`in`)) {
     } else if (a[2] != a[3]) {
         print(a[2] * a[0])
     } else {
-        if(a[1] == a[0])
+        if (a[1] == a[0])
             print(a[3] * a[0])
         else
             print(a[1] * a[0])
