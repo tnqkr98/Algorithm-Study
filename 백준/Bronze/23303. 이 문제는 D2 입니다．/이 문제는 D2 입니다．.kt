f13ece127eac60c39094b0551e32a7bb8daf2c8e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-25 00:07:27 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 172 ms, Memory: 16304 KB -BaekjoonHub

--- 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.kt
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.kt
@@ -2,6 +2,6 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val s = nextLine()
-    if(s.contains("D2") || s.contains("d2")) print("D2")
+    if (s.contains("D2") || s.contains("d2")) print("D2")
     else print("unrated")
 }
