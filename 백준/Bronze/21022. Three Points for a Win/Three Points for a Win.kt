6ba7537c2046a8eafe9c694a11c275b96b4630ef
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-27 00:50:09 +0900

    [Bronze III] Title: Three Points for a Win, Time: 136 ms, Memory: 13492 KB -BaekjoonHub

--- 백준/Bronze/21022. Three Points for a Win/Three Points for a Win.kt
+++ 백준/Bronze/21022. Three Points for a Win/Three Points for a Win.kt
@@ -4,11 +4,12 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var s = 0
     val a = Array(n) { nextInt() }
-    val b = Array(n) { nextInt()
+    val b = Array(n) {
+        nextInt()
     }
-    for(i in 0 until n) {
-        if(a[i] > b[i]) s+=3
-        else if(a[i] == b[i]) s+=1
+    for (i in 0 until n) {
+        if (a[i] > b[i]) s += 3
+        else if (a[i] == b[i]) s += 1
     }
     print(s)
 }
