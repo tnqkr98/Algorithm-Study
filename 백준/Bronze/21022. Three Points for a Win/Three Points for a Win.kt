974b43c64465692f48bd2bdd9a04f16935201a2b
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-27 00:49:42 +0900

    [Bronze III] Title: Three Points for a Win, Time: 128 ms, Memory: 13488 KB -BaekjoonHub

---
+++ 백준/Bronze/21022. Three Points for a Win/Three Points for a Win.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var s = 0
+    val a = Array(n) { nextInt() }
+    val b = Array(n) { nextInt()
+    }
+    for(i in 0 until n) {
+        if(a[i] > b[i]) s+=3
+        else if(a[i] == b[i]) s+=1
+    }
+    print(s)
+}
