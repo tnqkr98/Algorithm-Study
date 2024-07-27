45cebad4eec8f944b55c37ca26b081007e12b807
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-27 18:19:23 +0900

    [Bronze IV] Title: Cornhole, Time: 120 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Bronze/27855. Cornhole/Cornhole.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val h1 = nextInt()
+    val b1 = nextInt()
+    val s1 = h1 * 3 + b1
+    val h2 = nextInt()
+    val b2 = nextInt()
+    val s2 = h2 * 3 + b2
+    if(s1>s2) {
+        print("1 ${s1-s2}")
+    } else if(s2>s1){
+        print("2 ${s2-s1}")
+    } else {
+        print("NO SCORE")
+    }
+}
