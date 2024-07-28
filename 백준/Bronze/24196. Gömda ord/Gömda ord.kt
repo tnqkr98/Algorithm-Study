66bb7df2b8fd48ac89230fba027aa745e3f432fb
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-28 21:46:35 +0900

    [Bronze IV] Title: Gömda ord, Time: 100 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/24196. Gömda ord/Gömda ord.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val s = next()
+    var l = s[0]
+    var i = 0
+    while(true) {
+        print(l)
+        i += (l -'A'+1)
+        if(i >= s.length) break
+        l = s[i]
+    }
+}
