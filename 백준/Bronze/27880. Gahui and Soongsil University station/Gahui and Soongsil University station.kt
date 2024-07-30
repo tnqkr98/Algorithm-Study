771e3492fc8a5bf0a0a09431d8bcfa767bc8d044
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-30 22:47:45 +0900

    [Bronze IV] Title: Gahui and Soongsil University station, Time: 124 ms, Memory: 13508 KB -BaekjoonHub

--- 백준/Bronze/27880. Gahui and Soongsil University station/Gahui and Soongsil University station.kt
+++ 백준/Bronze/27880. Gahui and Soongsil University station/Gahui and Soongsil University station.kt
@@ -2,11 +2,11 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     var c = 0
-    for(i in 0 until 4) {
+    for (i in 0 until 4) {
         val s = next()
         val n = nextInt()
-        if(s == "Es") c+=n*21
-        else c+=n*17
+        if (s == "Es") c += n * 21
+        else c += n * 17
     }
     print(c)
 }
