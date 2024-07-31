4c84cce54f9b4401d42f6836a3d956e79e2c9e93
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-07-31 23:00:12 +0900

    [Bronze IV] Title: Advance to Taoyuan Regional, Time: 124 ms, Memory: 13456 KB -BaekjoonHub

--- 백준/Bronze/30319. Advance to Taoyuan Regional/Advance to Taoyuan Regional.kt
+++ 백준/Bronze/30319. Advance to Taoyuan Regional/Advance to Taoyuan Regional.kt
@@ -2,9 +2,9 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val s = next()
-    val m = s.substring(5,7).toInt()
-    val d = s.substring(8,10).toInt()
-    if(m == 9 && d <=16) print("GOOD")
-    else if(m<=8) print("GOOD")
+    val m = s.substring(5, 7).toInt()
+    val d = s.substring(8, 10).toInt()
+    if (m == 9 && d <= 16) print("GOOD")
+    else if (m <= 8) print("GOOD")
     else print("TOO LATE")
 }
