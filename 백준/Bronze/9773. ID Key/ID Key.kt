16a8f969b4ba4b9e0aa2c370b53a665a3a43f7fa
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-05 21:05:04 +0900

    [Bronze III] Title: ID Key, Time: 104 ms, Memory: 13636 KB -BaekjoonHub

--- 백준/Bronze/9773. ID Key/ID Key.kt
+++ 백준/Bronze/9773. ID Key/ID Key.kt
@@ -2,16 +2,16 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = next()
         var n1 = 0
         s.forEach {
-            n1+=it.digitToInt()
+            n1 += it.digitToInt()
         }
-        val n2 = (s.toLong()%1000)*10
-        val r = n1+n2
+        val n2 = (s.toLong() % 1000) * 10
+        val r = n1 + n2
         val rs = r.toString()
-        if(r>=1000) println(r.toString().substring(rs.length-4))
-        else println(r+1000)
+        if (r >= 1000) println(r.toString().substring(rs.length - 4))
+        else println(r + 1000)
     }
 }
