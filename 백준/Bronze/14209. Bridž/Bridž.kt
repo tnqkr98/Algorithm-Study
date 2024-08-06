84db1b0a331115f0f240729e88ea04686dc5ecd5
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-06 20:31:54 +0900

    [Bronze III] Title: Bridž, Time: 228 ms, Memory: 24424 KB -BaekjoonHub

--- 백준/Bronze/14209. Bridž/Bridž.kt
+++ 백준/Bronze/14209. Bridž/Bridž.kt
@@ -3,14 +3,14 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     val m = mapOf(
-        Pair('A',4),
-        Pair('K',3),
-        Pair('Q',2),
-        Pair('J',1),
-        Pair('X',0),
+        Pair('A', 4),
+        Pair('K', 3),
+        Pair('Q', 2),
+        Pair('J', 1),
+        Pair('X', 0),
     )
     var sum = 0
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = next()
         s.forEach {
             sum += m[it]!!
