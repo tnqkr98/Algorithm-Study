da819176ebee5b086f8777c59acf322f0e90483e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-09 21:53:09 +0900

    [Bronze III] Title: Report Card Time, Time: 96 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Bronze/11367. Report Card Time/Report Card Time.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    for(i in 0 until n) {
+        val s = next()
+        val m = nextInt()
+        print("$s ")
+        if(m >= 97) println("A+")
+        else if(m>=90) println("A")
+        else if(m>=87) println("B+")
+        else if(m>=80) println("B")
+        else if(m>=77) println("C+")
+        else if(m>=70) println("C")
+        else if(m>=67) println("D+")
+        else if(m>=60) println("D")
+        else if(m>=0) println("F")
+    }
+}
