2b3a54fd581242ca729e0f88226746d0efc6fb9a
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-11 21:23:35 +0900

    [Bronze III] Title: 숫자 맞추기 게임, Time: 120 ms, Memory: 13580 KB -BaekjoonHub

--- 백준/Bronze/4892. 숫자 맞추기 게임/숫자 맞추기 게임.kt
+++ 백준/Bronze/4892. 숫자 맞추기 게임/숫자 맞추기 게임.kt
@@ -4,14 +4,14 @@ fun main() = with(Scanner(System.`in`)) {
     var i = 1
     while (true) {
         val n0 = nextInt()
-        if(n0 == 0) break
+        if (n0 == 0) break
         val n1 = 3 * n0
         var n2 = 0
         n2 = if (n1 % 2 == 0) n1 / 2
         else (n1 + 1) / 2
         val n3 = 3 * n2
-        val n4 =n3/9
-        val s = if(n1 %2 == 0) "even" else "odd"
+        val n4 = n3 / 9
+        val s = if (n1 % 2 == 0) "even" else "odd"
         println("${i++}. $s $n4")
     }
 }
