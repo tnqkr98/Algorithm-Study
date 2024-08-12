c7eac343eac30378b299d00e387884e346e66016
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-12 21:39:30 +0900

    [Bronze III] Title: Left Beehind, Time: 96 ms, Memory: 13236 KB -BaekjoonHub

---
+++ 백준/Bronze/24768. Left Beehind/Left Beehind.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    while(true) {
+        val a = nextInt()
+        val b = nextInt()
+        if(a == 0 && b == 0) break
+
+        if(a == b) println("Undecided.")
+        else if(a+b == 13) println("Never speak again.")
+        else if(a>b) println("To the convention.")
+        else println("Left beehind.")
+    }
+}
