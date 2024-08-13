380a6ef00a261b84746caf1d8b4271c2e0820686
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-13 23:03:42 +0900

    [Bronze III] Title: Nadan, Time: 124 ms, Memory: 13260 KB -BaekjoonHub

---
+++ 백준/Bronze/16239. Nadan/Nadan.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val k = nextInt()
+    val n = nextInt()
+    var s = 0
+    for(i in 1  until n) {
+        println(i)
+        s+=i
+    }
+    println(k-s)
+}
