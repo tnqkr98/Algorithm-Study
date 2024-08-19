35af60cb8859a3c0fd6fdb8cc447eb106c16227d
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-19 23:05:52 +0900

    [Bronze III] Title: 스키테일 암호, Time: 108 ms, Memory: 13628 KB -BaekjoonHub

---
+++ 백준/Bronze/23080. 스키테일 암호/스키테일 암호.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val s = next()
+    s.forEachIndexed { idx, it ->
+        if(idx %n ==0) print(it)
+    }
+}
