a361431ee4de2d60123016241970569ce18fdacc
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-20 20:15:06 +0900

    [Bronze III] Title: Triangles, Time: 100 ms, Memory: 13228 KB -BaekjoonHub

---
+++ 백준/Bronze/5356. Triangles/Triangles.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val a = 'A'.code
+    for (i in 0 until n) {
+        val m = nextInt()
+        val c = next()[0].code
+        for (j in 0 until m) {
+            val t = (c - a + j) % 26 + a
+            for(k in 0 until j+1) {
+                print(t.toChar())
+            }
+            println()
+        }
+        println()
+    }
+}
