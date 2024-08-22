fb0d9d5436b67acf69e23c0b87aa52b9aca22cbf
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-08-22 22:23:35 +0900

    [Bronze III] Title: Heart Rate, Time: 384 ms, Memory: 27412 KB -BaekjoonHub

--- 백준/Bronze/15279. Heart Rate/Heart Rate.kt
+++ 백준/Bronze/15279. Heart Rate/Heart Rate.kt
@@ -3,17 +3,16 @@ import java.text.DecimalFormat
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
-
     val df = DecimalFormat("#.0000")
     df.roundingMode = RoundingMode.HALF_UP
 
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val b = nextInt().toDouble() // 횟수
         val p = nextDouble() // 초
-        val bpm = 60*b/p
-        val abpm1 = bpm - (bpm/b)
-        val abpm2 = bpm + (bpm/b)
+        val bpm = 60 * b / p
+        val abpm1 = bpm - (bpm / b)
+        val abpm2 = bpm + (bpm / b)
         println("${df.format(abpm1)} ${df.format(bpm)} ${df.format(abpm2)}")
     }
 }
