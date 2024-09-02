85cd7fdcc4092370662d774de92cd5d2c311ae78
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-03 00:05:04 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 272 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.kt
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.kt
@@ -4,12 +4,12 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var m = nextInt()
     var max = m
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val a = nextInt()
         val b = nextInt()
-        m+=a-b
-        if(m > max) max = m
-        else if(m < 0) {
+        m += a - b
+        if (m > max) max = m
+        else if (m < 0) {
             max = 0
             break
         }
