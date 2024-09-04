ef0ef508ab0bad0a86d796b3e10309e9f25c5ea3
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-05 01:25:35 +0900

    [Bronze III] Title: Angles, Time: 96 ms, Memory: 13308 KB -BaekjoonHub

---
+++ 백준/Bronze/15178. Angles/Angles.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    for(i in 0 until n) {
+        var s = 0
+        for(j in 0 until 3) {
+            val a = nextInt()
+            print("$a ")
+            s += a
+        }
+        if(s == 180) println("Seems OK")
+        else println("Check")
+    }
+}
