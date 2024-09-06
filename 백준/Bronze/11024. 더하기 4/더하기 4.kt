8728348bf977c2666ca8ec411b49212469b75b64
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-06 23:42:50 +0900

    [Bronze III] Title: 더하기 4, Time: 192 ms, Memory: 23256 KB -BaekjoonHub

--- 백준/Bronze/11024. 더하기 4/더하기 4.kt
+++ 백준/Bronze/11024. 더하기 4/더하기 4.kt
@@ -3,7 +3,7 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextLine().toInt()
 
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = nextLine().split(' ').map { it.toIntOrNull() }.mapNotNull { it }.sum()
         println(s)
     }
