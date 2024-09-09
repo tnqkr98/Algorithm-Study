fef4876bd441901a72a251376585a8628cbd3aac
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-10 02:56:06 +0900

    [Bronze IV] Title: Malvika is peculiar about color of balloons, Time: 116 ms, Memory: 13864 KB -BaekjoonHub

--- 백준/Bronze/30156. Malvika is peculiar about color of balloons/Malvika is peculiar about color of balloons.kt
+++ 백준/Bronze/30156. Malvika is peculiar about color of balloons/Malvika is peculiar about color of balloons.kt
@@ -2,14 +2,14 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = next()
         var a = 0
         var b = 0
         s.forEach {
-            if(it == 'a') a++
-            if(it == 'b') b++
+            if (it == 'a') a++
+            if (it == 'b') b++
         }
-        println("${if(a<b)a else b}")
+        println("${if (a < b) a else b}")
     }
 }
