febbc24ae5788ebd63d0b5aea5893fa4f1973eb9
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-12 00:02:10 +0900

    [Bronze III] Title: Vowel Count, Time: 104 ms, Memory: 13600 KB -BaekjoonHub

--- 백준/Bronze/26314. Vowel Count/Vowel Count.kt
+++ 백준/Bronze/26314. Vowel Count/Vowel Count.kt
@@ -2,16 +2,16 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = next()
         var v = 0
         var c = 0
         s.forEach {
-            if(it == 'a' || it == 'i' ||it == 'o' ||it == 'u' ||it == 'e') v++
+            if (it == 'a' || it == 'i' || it == 'o' || it == 'u' || it == 'e') v++
             else c++
         }
         println(s)
-        if(v > c) println(1)
+        if (v > c) println(1)
         else println(0)
     }
 }
