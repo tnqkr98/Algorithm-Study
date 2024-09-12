942fc83c96f1d1d20c9e2f1d9ffe1aef69226a54
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-13 00:15:26 +0900

    [Bronze III] Title: Donation Packaging, Time: 172 ms, Memory: 20644 KB -BaekjoonHub

--- 백준/Bronze/11795. Donation Packaging/Donation Packaging.kt
+++ 백준/Bronze/11795. Donation Packaging/Donation Packaging.kt
@@ -5,17 +5,17 @@ fun main() = with(Scanner(System.`in`)) {
     var a = 0
     var b = 0
     var c = 0
-    for(i in 0 until n) {
-        a+= nextInt()
-        b+= nextInt()
-        c+= nextInt()
-        val m = arrayOf(a,b,c).min()
-        if(m >= 30) {
+    for (i in 0 until n) {
+        a += nextInt()
+        b += nextInt()
+        c += nextInt()
+        val m = arrayOf(a, b, c).min()
+        if (m >= 30) {
             println(m)
-            a-=m
-            b-=m
-            c-=m
-        }else {
+            a -= m
+            b -= m
+            c -= m
+        } else {
             println("NO")
         }
     }
