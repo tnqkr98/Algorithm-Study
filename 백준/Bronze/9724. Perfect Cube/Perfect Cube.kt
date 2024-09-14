890a9452831a4864a2f828c00d6ca78d8113b51e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-15 00:18:04 +0900

    [Bronze III] Title: Perfect Cube, Time: 164 ms, Memory: 15632 KB -BaekjoonHub

--- 백준/Bronze/9724. Perfect Cube/Perfect Cube.kt
+++ 백준/Bronze/9724. Perfect Cube/Perfect Cube.kt
@@ -1,12 +1,12 @@
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
-    val arr = mutableMapOf<Int,Int>()
+    val arr = mutableMapOf<Int, Int>()
     var c = 0
-    for(i in 1 .. 2000000000) {
-        if(i*i*i <= 2000000000) {
+    for (i in 1..2000000000) {
+        if (i * i * i <= 2000000000) {
             c++
-            arr[i*i*i] = c
+            arr[i * i * i] = c
         } else {
             break
         }
@@ -15,21 +15,21 @@ fun main() = with(Scanner(System.`in`)) {
 
     val t = nextInt()
     val l = arr.toList()
-    for(i in 0 until t) {
+    for (i in 0 until t) {
         val a = nextInt()
         val b = nextInt()
         var av = 0
         var bv = 0
 
-       l.forEach {  p->
-            if(p.first < a) {
+        l.forEach { p ->
+            if (p.first < a) {
                 av = p.second
             }
-            if(p.first <= b) {
+            if (p.first <= b) {
                 bv = p.second
             }
         }
 
-        println("Case #${i+1}: ${bv-av}")
+        println("Case #${i + 1}: ${bv - av}")
     }
 }
