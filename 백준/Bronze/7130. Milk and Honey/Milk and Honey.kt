65ae135f0829b9d37544e3d263f66ad795769d59
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-18 00:04:27 +0900

    [Bronze III] Title: Milk and Honey, Time: 100 ms, Memory: 13464 KB -BaekjoonHub

---
+++ 백준/Bronze/7130. Milk and Honey/Milk and Honey.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+    val n = nextInt()
+    var s = 0
+    for(i in 0 until n) {
+        val t1 = nextInt()
+        val t2 = nextInt()
+        s += if(t1*a < t2*b) {
+            t2*b
+        } else {
+            t1*a
+        }
+    }
+    print(s)
+}
