58d66097e7bce21e67c1e52444c565d0e2630ea1
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-20 00:44:16 +0900

    [Bronze IV] Title: Affischutskicket, Time: 104 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Bronze/24183. Affischutskicket/Affischutskicket.kt
+++ 백준/Bronze/24183. Affischutskicket/Affischutskicket.kt
@@ -2,11 +2,11 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val c4 = 0.229 * 0.324 * 2
-    val a3 = 0.297 * 0.42 *2
+    val a3 = 0.297 * 0.42 * 2
     val a4 = 0.21 * 0.297
     val w1 = nextDouble()
     val w2 = nextDouble()
     val w3 = nextDouble()
-    val r = c4*w1 + a3*w2 + a4*w3
+    val r = c4 * w1 + a3 * w2 + a4 * w3
     print(r)
 }
