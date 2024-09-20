65103808494a8d4a4fa92777916fedfc36739b6f
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-20 23:29:39 +0900

    [Bronze III] Title: Stopwatch, Time: 132 ms, Memory: 14996 KB -BaekjoonHub

--- 백준/Bronze/21197. Stopwatch/Stopwatch.kt
+++ 백준/Bronze/21197. Stopwatch/Stopwatch.kt
@@ -2,17 +2,17 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    if(n%2 == 1) {
+    if (n % 2 == 1) {
         print("still running")
         return@with
     }
     var b = nextInt()
     var s = 0
-    for(i in 1 until n) {
-        if(i%2 ==0) b = nextInt()
+    for (i in 1 until n) {
+        if (i % 2 == 0) b = nextInt()
         else {
             val c = nextInt()
-            s+=c-b
+            s += c - b
         }
     }
     print(s)
