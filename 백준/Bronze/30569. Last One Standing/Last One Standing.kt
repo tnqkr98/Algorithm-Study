e25aed8a2144a6fd43aa677f9fd6a65e6ae0a253
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-22 16:16:13 +0900

    [Bronze III] Title: Last One Standing, Time: 108 ms, Memory: 13272 KB -BaekjoonHub

---
+++ 백준/Bronze/30569. Last One Standing/Last One Standing.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var h1 = nextInt()
+    val d1 = nextInt()
+    val t1 = nextInt()
+
+    var h2 = nextInt()
+    val d2 = nextInt()
+    val t2 = nextInt()
+
+    h1 -= d2
+    h2 -= d1
+    var t = 1
+    while(h1 >0 && h2>0) {
+        if(t % t1 ==0) {
+            h2 -= d1
+        }
+
+        if(t % t2 ==0) {
+            h1 -= d2
+        }
+
+        t++
+    }
+
+    if(h1 <=0 && h2<=0) print("draw")
+    else if(h1 <=0) print("player two")
+    else print("player one")
+}
