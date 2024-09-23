9d31aa9195283e6e61bc68692ea0c5574f4d0d33
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-23 21:03:20 +0900

    [Bronze IV] Title: 선택 가능성이 가장 높은 자료형, Time: 96 ms, Memory: 13252 KB -BaekjoonHub

--- 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.kt
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.kt
@@ -2,11 +2,11 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextLong()
-    if(n >= -32768 && n <= 32767) {
+    if (n >= -32768 && n <= 32767) {
         print("short")
-    } else if(n >= -2147483648 && n <= 2147483647){
+    } else if (n >= -2147483648 && n <= 2147483647) {
         print("int")
-    } else if(n >= "-9223372036854775808".toLong() && n <= "9223372036854775807".toLong()){
+    } else if (n >= "-9223372036854775808".toLong() && n <= "9223372036854775807".toLong()) {
         print("long long")
     }
 }
