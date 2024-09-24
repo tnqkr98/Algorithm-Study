68e35a6f4c7308a2ab8478e224d2365505d878dc
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-24 22:02:58 +0900

    [Bronze III] Title: Farmers’ Market, Time: 96 ms, Memory: 13232 KB -BaekjoonHub

--- 백준/Bronze/32305. Farmers’ Market/Farmers’ Market.kt
+++ 백준/Bronze/32305. Farmers’ Market/Farmers’ Market.kt
@@ -4,9 +4,9 @@ fun main() = with(Scanner(System.`in`)) {
     val a = nextInt()
     val b = nextInt()
     val d = nextInt()
-    if(a*b%12 == 0) {
-        print((a*b/12)*d)
+    if (a * b % 12 == 0) {
+        print((a * b / 12) * d)
     } else {
-        print((a*b/12)*d + d)
+        print((a * b / 12) * d + d)
     }
 }
