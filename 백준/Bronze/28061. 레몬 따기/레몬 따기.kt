e3c71a02b7b8722a4d0f71ef9daf6c9048bca400
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-26 00:10:58 +0900

    [Bronze III] Title: 레몬 따기, Time: 816 ms, Memory: 168480 KB -BaekjoonHub

---
+++ 백준/Bronze/28061. 레몬 따기/레몬 따기.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var m = -9999999
+    for(i in 1 .. n) {
+        val a = nextInt()
+        val t = a - (n+1-i)
+        if(t>m) {
+            m = t
+        }
+    }
+    print(m)
+}
