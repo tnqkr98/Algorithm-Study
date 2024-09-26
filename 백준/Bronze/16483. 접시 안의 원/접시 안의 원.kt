969b987f2beb1e12a9b8244d1c1b54a6a48a0935
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-26 23:16:59 +0900

    [Bronze III] Title: 접시 안의 원, Time: 108 ms, Memory: 15152 KB -BaekjoonHub

--- 백준/Bronze/16483. 접시 안의 원/접시 안의 원.kt
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.kt
@@ -3,6 +3,6 @@ import kotlin.math.roundToInt
 
 fun main() = with(Scanner(System.`in`)) {
     val t = nextDouble()
-    val r = ((t/2)*(t/2)).roundToInt()
+    val r = ((t / 2) * (t / 2)).roundToInt()
     print(r)
 }
