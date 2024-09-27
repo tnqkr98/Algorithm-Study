a1b6f1ae6491f60432a936c704fb96fe43fb6fd0
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-28 00:12:15 +0900

    [Bronze III] Title: Chessboard, Time: 100 ms, Memory: 13252 KB -BaekjoonHub

--- 백준/Bronze/13240. Chessboard/Chessboard.kt
+++ 백준/Bronze/13240. Chessboard/Chessboard.kt
@@ -4,11 +4,11 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     val m = nextInt()
     var b = 0
-    for(i in 0 until n) {
-        if(i %2 == 0) b = 0
+    for (i in 0 until n) {
+        if (i % 2 == 0) b = 0
         else b = 1
-        for(j in 0 until m) {
-            if(b == 0) {
+        for (j in 0 until m) {
+            if (b == 0) {
                 print('*')
                 b = 1
             } else {
