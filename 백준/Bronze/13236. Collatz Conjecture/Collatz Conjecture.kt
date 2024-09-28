650ce0f57c9d2083a1bc26c3a0d1299ef89227ac
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-09-28 15:36:49 +0900

    [Bronze III] Title: Collatz Conjecture, Time: 104 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Bronze/13236. Collatz Conjecture/Collatz Conjecture.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var n = nextInt()
+    while (n != 1) {
+        print("$n ")
+        if(n %2 ==0) n/=2
+        else n =n*3+1
+    }
+    print(1)
+}
