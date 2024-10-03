8c35f4cb7999fb4a0b8adb3d5e70abea00e9fb50
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-03 13:32:25 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 104 ms, Memory: 13284 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val a = nextInt()
+    println(a)
+    for(i in 0 until a) {
+        val s = next()
+        println(s)
+    }
+}
