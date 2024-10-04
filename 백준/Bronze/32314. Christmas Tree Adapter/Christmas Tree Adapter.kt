bcf9bfb457426d84c9f0df41486069cf5adaad81
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-04 21:07:15 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 100 ms, Memory: 13240 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val w = nextInt()
+    val v = nextInt()
+    val r = w/v
+    if(a <= r) {
+        print(1)
+    } else {
+        print(0)
+    }
+}
