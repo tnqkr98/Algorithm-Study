fdcbb9da60c49b7520b172040ec803fd0d38ba8d
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-07 21:00:07 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 92 ms, Memory: 13240 KB -BaekjoonHub

--- 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.kt
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.kt
@@ -2,8 +2,8 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n)  {
+    for (i in 0 until n) {
         print("LoveisKoreaUniversity")
-        if(i != n-1) print(" ")
+        if (i != n - 1) print(" ")
     }
 }
