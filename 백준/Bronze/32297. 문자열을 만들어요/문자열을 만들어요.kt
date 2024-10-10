69780656891c795f99ef1d916263855992518837
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-10 23:44:53 +0900

    [Bronze III] Title: 문자열을 만들어요, Time: 116 ms, Memory: 15284 KB -BaekjoonHub

---
+++ 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    nextInt()
+    val s = next()
+    if(s.contains("gori")) {
+        print("YES")
+    } else {
+        print("NO")
+    }
+}
