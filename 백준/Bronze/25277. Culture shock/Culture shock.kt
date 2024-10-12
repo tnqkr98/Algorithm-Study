924727f753ad100cc011859ef088bfc0dd368295
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-13 00:19:00 +0900

    [Bronze III] Title: Culture shock, Time: 504 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Bronze/25277. Culture shock/Culture shock.kt
+++ 백준/Bronze/25277. Culture shock/Culture shock.kt
@@ -3,9 +3,9 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var c = 0
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = next()
-        if(s == "he" || s == "him" || s == "she" || s == "her") c++
+        if (s == "he" || s == "him" || s == "she" || s == "her") c++
     }
     print(c)
 }
