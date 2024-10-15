46415c27e6413ee047b2ad889b8d74c4905d678e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-15 22:43:45 +0900

    [Bronze III] Title: Санта Клаус, Time: 104 ms, Memory: 13444 KB -BaekjoonHub

--- 백준/Bronze/19963. Санта Клаус/Санта Клаус.kt
+++ 백준/Bronze/19963. Санта Клаус/Санта Клаус.kt
@@ -5,14 +5,14 @@ fun main() = with(Scanner(System.`in`)) {
     val b1 = nextInt()
     val b2 = nextInt()
     val set = mutableSetOf<Int>()
-    for(i in 1 .. n) {
+    for (i in 1..n) {
         set.add(i)
     }
-    for(i in 0 until b1) {
+    for (i in 0 until b1) {
         val t = nextInt()
         set.remove(t)
     }
-    for(i in 0 until b2) {
+    for (i in 0 until b2) {
         val t = nextInt()
         set.remove(t)
     }
