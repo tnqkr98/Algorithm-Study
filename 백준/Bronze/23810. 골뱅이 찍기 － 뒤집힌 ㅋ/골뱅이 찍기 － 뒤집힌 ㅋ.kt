aa94df497b6ed120f1462ef24348de30b8044c43
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-20 13:32:35 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 744 ms, Memory: 22412 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    for (i in 1..n * 5) {
+        for (j in 1..n * 5) {
+            if (i <= n || (i > n * 2 && i <= n * 3)) {
+                print("@")
+            } else {
+                if (j <= n)
+                    print("@")
+                //else
+                    //print(" ")
+            }
+        }
+        println()
+    }
+}
