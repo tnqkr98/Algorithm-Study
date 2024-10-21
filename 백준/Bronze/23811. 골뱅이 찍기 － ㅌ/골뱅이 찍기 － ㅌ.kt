53cbc0b6759826a3cb7b8cbf07ccea9733ed8621
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-21 20:53:12 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 800 ms, Memory: 23804 KB -BaekjoonHub

--- 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.kt
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.kt
@@ -4,7 +4,7 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     for (i in 1..n * 5) {
         for (j in 1..n * 5) {
-            if (i <= n || (i > n * 2 && i <= n * 3)|| (i > n * 4 && i <= n * 5)) {
+            if (i <= n || (i > n * 2 && i <= n * 3) || (i > n * 4 && i <= n * 5)) {
                 print("@")
             } else {
                 if (j <= n)
