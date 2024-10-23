a1153e84289003953e3858065b0203b9a4d13707
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-24 00:58:35 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 1120 ms, Memory: 27744 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    for (i in 1..n * 5) {
+        for (j in 1..n * 5) {
+            if (i <= n) {
+                if (j <= n * 3 || j > n * 4)
+                    print("@")
+                else
+                    print(" ")
+            }else if(i>n*4) {
+                if (j <= n || j > n * 2)
+                    print("@")
+                else
+                    print(" ")
+            } else {
+                if (j <= n || (j > n * 2 && j <= n*3) || j>n*4)
+                    print("@")
+                else
+                    print(" ")
+            }
+        }
+        println()
+    }
+}
