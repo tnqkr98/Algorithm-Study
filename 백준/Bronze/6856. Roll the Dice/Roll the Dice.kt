1e17dcc78036c8282b015d94d371967799ac9093
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-10-31 00:02:36 +0900

    [Bronze III] Title: Roll the Dice, Time: 100 ms, Memory: 13228 KB -BaekjoonHub

--- 백준/Bronze/6856. Roll the Dice/Roll the Dice.kt
+++ 백준/Bronze/6856. Roll the Dice/Roll the Dice.kt
@@ -4,12 +4,12 @@ fun main() = with(Scanner(System.`in`)) {
     val a = nextInt()
     val b = nextInt()
     var c = 0
-    for(i in 1 .. a) {
-        for(j in 1 .. b){
-            if(i+j == 10) c++
+    for (i in 1..a) {
+        for (j in 1..b) {
+            if (i + j == 10) c++
         }
     }
-    if(c == 1) {
+    if (c == 1) {
         print("There is 1 way to get the sum 10.")
     } else {
         print("There are $c ways to get the sum 10.")
