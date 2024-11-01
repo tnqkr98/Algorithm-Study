1676235e4c8128ed929dc18ef3315fd0d8ce680c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-02 00:14:02 +0900

    [Bronze III] Title: Axis-Aligned Area, Time: 104 ms, Memory: 13232 KB -BaekjoonHub

---
+++ 백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val a1 = nextInt()
+    val a2 = nextInt()
+    val a3 = nextInt()
+    val a4 = nextInt()
+    print("${(if(a4>a3)a3 else a4)*(if(a2>a1)a1 else a2)}")
+}
