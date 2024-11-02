b17949c9fc927f385a50a378cecb1faf5465ab85
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-02 22:33:37 +0900

    [Bronze III] Title: Смена стиля, Time: 728 ms, Memory: 25668 KB -BaekjoonHub

--- 백준/Bronze/28637. Смена стиля/Смена стиля.kt
+++ 백준/Bronze/28637. Смена стиля/Смена стиля.kt
@@ -2,14 +2,14 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s = next()
         s.forEachIndexed { idx, it ->
             var c = it
-            if(idx == 0) {
+            if (idx == 0) {
                 c = it.lowercaseChar()
             } else {
-                if(it.isUpperCase()) {
+                if (it.isUpperCase()) {
                     print("_")
                     c = it.lowercaseChar()
                 }
