e14c14865d8fbf8f2d107264de876948e141f151
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-03 23:16:05 +0900

    [Bronze III] Title: Закат, Time: 116 ms, Memory: 13748 KB -BaekjoonHub

---
+++ 백준/Bronze/29308. Закат/Закат.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var max = 0
+    var maxs = ""
+    for(i in 0 until n) {
+        val m = nextInt()
+        val s = next()
+        val c = next()
+        if(c == "Russia") {
+            if(max < m) {
+                max = m
+                maxs = s
+            }
+        }
+    }
+    print(maxs)
+}
