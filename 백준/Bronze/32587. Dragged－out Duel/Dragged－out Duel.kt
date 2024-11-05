6096d353b72056211464d53025b3c047f0ccb004
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-05 23:14:03 +0900

    [Bronze III] Title: Dragged-out Duel, Time: 128 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/32587. Dragged－out Duel/Dragged－out Duel.kt
+++ 백준/Bronze/32587. Dragged－out Duel/Dragged－out Duel.kt
@@ -7,12 +7,12 @@ fun main() = with(Scanner(System.`in`)) {
     var v = 0
     var d = 0
     for (i in 0 until n) {
-        if(a[i] == b[i]) d++
+        if (a[i] == b[i]) d++
         if (a[i] == 'S' && b[i] == 'P') v++
         if (a[i] == 'P' && b[i] == 'R') v++
         if (a[i] == 'R' && b[i] == 'S') v++
     }
 
-    if (v > (n-d) / 2) print("victory")
+    if (v > (n - d) / 2) print("victory")
     else print("defeat")
 }
