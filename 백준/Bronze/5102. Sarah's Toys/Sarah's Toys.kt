77ac16c1e29c66c789918557021bc2248ac8c446
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-07 22:51:45 +0900

    [Bronze III] Title: Sarah's Toys, Time: 96 ms, Memory: 13220 KB -BaekjoonHub

---
+++ 백준/Bronze/5102. Sarah's Toys/Sarah's Toys.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    while(true) {
+        val a = nextInt()
+        val b = nextInt()
+        val r = a - b
+        if(a == 0 && b ==0) break
+        if(r <= 1) {
+            println("0 0")
+            continue
+        }
+        if(r%2 == 0) {
+            println("${r/2} 0")
+        } else {
+            println("${r/2-1} 1")
+        }
+    }
+}
