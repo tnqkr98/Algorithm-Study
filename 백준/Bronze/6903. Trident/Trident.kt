369421d7052ab26dbc91c0a3cb1b95fa1a7cbe36
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-08 23:20:04 +0900

    [Bronze III] Title: Trident, Time: 100 ms, Memory: 13292 KB -BaekjoonHub

--- 백준/Bronze/6903. Trident/Trident.kt
+++ 백준/Bronze/6903. Trident/Trident.kt
@@ -5,15 +5,15 @@ fun main() = with(Scanner(System.`in`)) {
     val s = nextInt()
     val h = nextInt()
 
-    for (i in 0 .. t + h) {
-        if(i<=t) print("*")
+    for (i in 0..t + h) {
+        if (i <= t) print("*")
         else print(" ")
         for (j in 0 until s) {
             if (i != t) print(" ")
             else print("*")
         }
         print("*")
-        if(i<=t) {
+        if (i <= t) {
             for (j in 0 until s) {
                 if (i != t) print(" ")
                 else print("*")
@@ -23,5 +23,4 @@ fun main() = with(Scanner(System.`in`)) {
             println()
         }
     }
-
 }
