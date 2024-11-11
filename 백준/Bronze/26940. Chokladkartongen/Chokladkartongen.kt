1481435a7c1d4d2d9486e424a82a8da3e0c4c86e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-11 22:46:30 +0900

    [Bronze III] Title: Chokladkartongen, Time: 104 ms, Memory: 13372 KB -BaekjoonHub

--- 백준/Bronze/26940. Chokladkartongen/Chokladkartongen.kt
+++ 백준/Bronze/26940. Chokladkartongen/Chokladkartongen.kt
@@ -3,10 +3,10 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var b = nextInt()
-    var c =0
-    for(i in 0 until n-1) {
+    var c = 0
+    for (i in 0 until n - 1) {
         val t = nextInt()
-        if(t>b) c++
+        if (t > b) c++
         b = t
     }
     print(c)
