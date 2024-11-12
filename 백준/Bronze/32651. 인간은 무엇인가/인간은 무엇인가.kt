786656f1a6e24cde2fc8136f2844285697c7ee31
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-12 23:00:07 +0900

    [Bronze IV] Title: 인간은 무엇인가, Time: 100 ms, Memory: 13240 KB -BaekjoonHub

--- 백준/Bronze/32651. 인간은 무엇인가/인간은 무엇인가.kt
+++ 백준/Bronze/32651. 인간은 무엇인가/인간은 무엇인가.kt
@@ -2,6 +2,6 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    if(n % 2024 == 0 && n <= 100000) print("Yes")
+    if (n % 2024 == 0 && n <= 100000) print("Yes")
     else print("No")
 }
