ae52c4122231681afcfa74bf44163242d546e8d7
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-18 23:53:49 +0900

    [Bronze III] Title: Which Number Kind Is It?, Time: 1716 ms, Memory: 98736 KB -BaekjoonHub

--- 백준/Bronze/32682. Which Number Kind Is It？/Which Number Kind Is It？.kt
+++ 백준/Bronze/32682. Which Number Kind Is It？/Which Number Kind Is It？.kt
@@ -3,14 +3,14 @@ import kotlin.math.sqrt
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val a = nextInt()
         val s = sqrt(a.toDouble())
-        if(a % 2 == 1 && s.toInt().toDouble() == s) {
+        if (a % 2 == 1 && s.toInt().toDouble() == s) {
             println("OS")
-        } else if(a % 2 == 1) {
+        } else if (a % 2 == 1) {
             println("O")
-        }else if(s.toInt().toDouble() == s) {
+        } else if (s.toInt().toDouble() == s) {
             println("S")
         } else {
             println("EMPTY")
