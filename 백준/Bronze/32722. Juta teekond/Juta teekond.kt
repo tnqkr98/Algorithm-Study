b80e94e1014b1ff99e1a958718365c12b0c4269a
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-20 01:01:45 +0900

    [Bronze IV] Title: Juta teekond, Time: 96 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+    val c = nextInt()
+    if((a == 1 || a== 3) && (b == 6 || b== 8) && (c == 2 || c== 5)) {
+        print("JAH")
+    } else {
+    print("EI")
+}
+
+}
