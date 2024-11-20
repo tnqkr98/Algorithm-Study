dd8ef478aee399301412caa86aa00d67b0ad9985
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-21 00:23:01 +0900

    [Bronze IV] Title: 장기, Time: 96 ms, Memory: 13392 KB -BaekjoonHub

---
+++ 백준/Bronze/32684. 장기/장기.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val s = arrayOf(13,7,5,3,3,2)
+    var s1 = 0
+    for(i in 0 until 6) {
+        val a = nextInt()
+        s1 += a*s[i]
+    }
+    var s2 = 1.5
+    for(i in 0 until 6) {
+        val a = nextInt()
+        s2 += a*s[i]
+    }
+    if(s1 > s2) print("cocjr0208")
+    else print("ekwoo")
+}
