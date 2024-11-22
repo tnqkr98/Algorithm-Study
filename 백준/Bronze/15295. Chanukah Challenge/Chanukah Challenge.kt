d636a3fedc370d5b33277485d954241c41bbd7cd
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-23 00:37:12 +0900

    [Bronze III] Title: Chanukah Challenge, Time: 540 ms, Memory: 36148 KB -BaekjoonHub

--- 백준/Bronze/15295. Chanukah Challenge/Chanukah Challenge.kt
+++ 백준/Bronze/15295. Chanukah Challenge/Chanukah Challenge.kt
@@ -2,11 +2,11 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val d = nextInt()
         val a = nextInt()
         var s = 0
-        for(j in 1 .. a) {
+        for (j in 1..a) {
             s += j + 1
         }
         println("$d $s")
