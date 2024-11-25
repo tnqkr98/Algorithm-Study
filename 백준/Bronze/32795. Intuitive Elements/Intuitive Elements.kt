c669714839d42d043662da6063a1166eb3d2e6c1
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-26 02:14:06 +0900

    [Bronze III] Title: Intuitive Elements, Time: 260 ms, Memory: 23628 KB -BaekjoonHub

--- 백준/Bronze/32795. Intuitive Elements/Intuitive Elements.kt
+++ 백준/Bronze/32795. Intuitive Elements/Intuitive Elements.kt
@@ -2,13 +2,13 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val s1 = next()
         val s2 = next()
         val isAll = s2.all {
             s1.contains(it)
         }
-        if(isAll) println("YES")
+        if (isAll) println("YES")
         else println("NO")
     }
 }
