7394f6d2a26ffcedde34b207fe0afcf87b57ace9
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-27 01:31:19 +0900

    [Bronze IV] Title: 구구단표, Time: 100 ms, Memory: 13292 KB -BaekjoonHub

--- 백준/Bronze/32710. 구구단표/구구단표.kt
+++ 백준/Bronze/32710. 구구단표/구구단표.kt
@@ -3,9 +3,9 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     val set = mutableSetOf<Int>()
-    for(i in 2 .. 9) {
-        for(j in 1 .. 9) {
-            set.add(i*j)
+    for (i in 2..9) {
+        for (j in 1..9) {
+            set.add(i * j)
         }
     }
     if (n <= 10 || set.contains(n)) print(1)
