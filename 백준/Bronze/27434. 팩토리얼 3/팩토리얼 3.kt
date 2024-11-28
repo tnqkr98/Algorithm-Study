2073b3cda035c81652750cbc0923303a548cc44f
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-28 23:36:34 +0900

    [Bronze V] Title: 팩토리얼 3, Time: 3132 ms, Memory: 369132 KB -BaekjoonHub

---
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.kt
@@ -0,0 +1,16 @@
+import java.math.BigInteger
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextLong()
+    print(fact(1,n))
+}
+
+fun fact(a: Long, n: Long): BigInteger {
+    var num = BigInteger.valueOf(a)
+    if(a < n) {
+        val b = (a+n)/2
+        num = fact(a,b) * fact(b+1,n)
+    }
+    return num
+}
