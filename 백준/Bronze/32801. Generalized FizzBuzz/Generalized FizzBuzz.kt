855b77700a703c91d4d0db16e02c4a9912453dce
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-11-30 01:56:57 +0900

    [Bronze IV] Title: Generalized FizzBuzz, Time: 128 ms, Memory: 13680 KB -BaekjoonHub

---
+++ 백준/Bronze/32801. Generalized FizzBuzz/Generalized FizzBuzz.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val a = nextInt()
+    val b = nextInt()
+
+    var fb = 0
+    var ff = 0
+    var bb = 0
+    for(i in 1 .. n) {
+        if (i % a == 0 && i % b == 0) {
+            fb++
+        } else if (i % a == 0) {
+            ff++
+        } else if (i % b == 0){
+            bb++
+        }
+    }
+    print("$ff $bb $fb")
+}
