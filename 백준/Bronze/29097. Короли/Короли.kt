17b1dff66063f5fa973ec41da10ba02681e6f96c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-05 01:48:33 +0900

    [Bronze IV] Title: Короли, Time: 152 ms, Memory: 20616 KB -BaekjoonHub

---
+++ 백준/Bronze/29097. Короли/Короли.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val m = nextInt()
+    val k = nextInt()
+    val a = nextInt()
+    val b = nextInt()
+    val c = nextInt()
+    val s = arrayOf("Joffrey", "Robb", "Stannis").sorted()
+    val map = mapOf(
+        "Joffrey" to n*a,
+        "Robb" to m*b,
+        "Stannis" to k*c
+    )
+    val max = arrayOf(n*a,m*b,k*c).max()
+
+    s.forEach {
+        if(map[it] == max) print("$it ")
+    }
+
+}
