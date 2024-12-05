376352ec5f8e7a69d245daeab36c5a5f481b75c4
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-06 01:37:41 +0900

    [Bronze III] Title: 코인의 신 건모, Time: 112 ms, Memory: 14916 KB -BaekjoonHub

--- 백준/Bronze/32866. 코인의 신 건모/코인의 신 건모.kt
+++ 백준/Bronze/32866. 코인의 신 건모/코인의 신 건모.kt
@@ -3,7 +3,6 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val w = nextDouble()
     val x = nextDouble()
-
     val cw = w - w * (x / 100.0)
     print((w - cw) / (cw / 100.0))
 }
