1fab149681543df87961419d767614b2994ce5cc
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-07 16:52:21 +0900

    [Bronze IV] Title: RACI, Time: 164 ms, Memory: 19156 KB -BaekjoonHub

--- 백준/Bronze/32905. RACI/RACI.kt
+++ 백준/Bronze/32905. RACI/RACI.kt
@@ -4,14 +4,14 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     val m = nextInt()
     var c = 0
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         var ac = 0
-        for(j in 0 until m) {
+        for (j in 0 until m) {
             val s = next()
-            if(s == "A") ac++
+            if (s == "A") ac++
         }
-        if(ac == 1) c++
+        if (ac == 1) c++
     }
-    if(n == c) print("Yes")
+    if (n == c) print("Yes")
     else print("No")
 }
