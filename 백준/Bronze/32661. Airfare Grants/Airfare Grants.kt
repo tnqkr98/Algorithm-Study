0a321ceaf6566c76bee4dccb82ecd4eb3c339d86
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-09 22:10:15 +0900

    [Bronze III] Title: Airfare Grants, Time: 104 ms, Memory: 13296 KB -BaekjoonHub

---
+++ 백준/Bronze/32661. Airfare Grants/Airfare Grants.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var max = 0
+    var min = 100001
+    for(i in 0 until n) {
+        val p = nextInt()
+        if(max < p) {
+            max = p
+        }
+        if(min > p) {
+            min = p
+        }
+    }
+
+    if(min - max/2 >=0) print(min - max/2)
+    else print(0)
+}
