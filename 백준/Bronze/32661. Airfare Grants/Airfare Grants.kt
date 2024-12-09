4d42aa5e6fb65b6984e9bdd546e4efae107a1e9d
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-09 22:10:54 +0900

    [Bronze III] Title: Airfare Grants, Time: 104 ms, Memory: 13284 KB -BaekjoonHub

--- 백준/Bronze/32661. Airfare Grants/Airfare Grants.kt
+++ 백준/Bronze/32661. Airfare Grants/Airfare Grants.kt
@@ -4,16 +4,16 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var max = 0
     var min = 100001
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val p = nextInt()
-        if(max < p) {
+        if (max < p) {
             max = p
         }
-        if(min > p) {
+        if (min > p) {
             min = p
         }
     }
 
-    if(min - max/2 >=0) print(min - max/2)
+    if (min - max / 2 >= 0) print(min - max / 2)
     else print(0)
 }
