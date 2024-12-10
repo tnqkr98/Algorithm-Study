2d8ad46059819ec0edb99a23fad9b81e57f54e33
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-11 01:50:40 +0900

    [Bronze V] Title: UOS 문자열, Time: 100 ms, Memory: 13248 KB -BaekjoonHub

--- 백준/Bronze/32929. UOS 문자열/UOS 문자열.kt
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
-    if(n%3 == 1) print("U")
-    else if(n%3 == 2) print("O")
+    if (n % 3 == 1) print("U")
+    else if (n % 3 == 2) print("O")
     else print("S")
 }
