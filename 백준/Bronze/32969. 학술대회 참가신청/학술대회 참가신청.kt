0e8de6a94f042f948c420f9d82e9c3a8fff864f3
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-17 00:11:58 +0900

    [Bronze III] Title: 학술대회 참가신청, Time: 128 ms, Memory: 16924 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val s = nextLine().split(" ")
+    if(s.contains("social") || s.contains("history") || s.contains("language") || s.contains("literacy")) {
+        print("digital humanities")
+    } else {
+        print("public bigdata")
+    }
+}
