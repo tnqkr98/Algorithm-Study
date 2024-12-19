67b947d3152bf7d2d1c6bad22970667886148f3c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-20 04:26:05 +0900

    [Bronze III] Title: Consolidating Windows, Time: 104 ms, Memory: 13856 KB -BaekjoonHub

--- 백준/Bronze/32888. Consolidating Windows/Consolidating Windows.kt
+++ 백준/Bronze/32888. Consolidating Windows/Consolidating Windows.kt
@@ -6,7 +6,7 @@ import kotlin.math.sqrt
 fun main() = with(Scanner(System.`in`)) {
     val a = nextInt().toBigInteger()
     val b = nextInt().toBigInteger()
-    val s = a*a+b*b
+    val s = a * a + b * b
     val r = sqrt(s.toDouble())
     val d = BigDecimal(r).setScale(9, RoundingMode.HALF_UP)
     println(d)
