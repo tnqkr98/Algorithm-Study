65a014bf904094b10f172ac466adcc3b18bf3ac1
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-21 01:56:41 +0900

    [Bronze III] Title: Bus Assignment, Time: 932 ms, Memory: 295004 KB -BaekjoonHub

---
+++ 백준/Bronze/32800. Bus Assignment/Bus Assignment.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    var c = 0
+    var max = 0
+    for(i in 0 until n) {
+        val a = nextInt()
+        val b = nextInt()
+        c -= a
+        c += b
+        if(max < c) {
+            max = c
+        }
+    }
+    print(max)
+}
