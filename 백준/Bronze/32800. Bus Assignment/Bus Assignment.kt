bd7d700fca652f0da5ffa273201ac68514dfc1c0
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-21 01:57:32 +0900

    [Bronze III] Title: Bus Assignment, Time: 936 ms, Memory: 294664 KB -BaekjoonHub

--- 백준/Bronze/32800. Bus Assignment/Bus Assignment.kt
+++ 백준/Bronze/32800. Bus Assignment/Bus Assignment.kt
@@ -4,12 +4,12 @@ fun main() = with(Scanner(System.`in`)) {
     val n = nextInt()
     var c = 0
     var max = 0
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val a = nextInt()
         val b = nextInt()
         c -= a
         c += b
-        if(max < c) {
+        if (max < c) {
             max = c
         }
     }
