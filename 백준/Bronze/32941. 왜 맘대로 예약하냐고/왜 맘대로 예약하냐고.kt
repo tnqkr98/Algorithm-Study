14f879df6457cabae4ebc60010f88df3aa2e7025
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-24 04:49:41 +0900

    [Bronze IV] Title: 왜 맘대로 예약하냐고, Time: 248 ms, Memory: 29308 KB -BaekjoonHub

--- 백준/Bronze/32941. 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.kt
+++ 백준/Bronze/32941. 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.kt
@@ -5,16 +5,16 @@ fun main() = with(Scanner(System.`in`)) {
     val x = nextInt()
     val n = nextInt()
     var b = true
-    for(i in 0 until n) {
+    for (i in 0 until n) {
         val g = nextInt()
         val arr = Array(g) {
             nextInt()
         }
-        if(!arr.contains(x)) {
+        if (!arr.contains(x)) {
             b = false
             break
         }
     }
-    if(b) print("YES")
+    if (b) print("YES")
     else print("NO")
 }
