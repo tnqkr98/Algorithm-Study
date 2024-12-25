63cc81d6224d08fcd9c226b444d76e3710965d17
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-26 01:49:52 +0900

    [Bronze III] Title: Autobusas, Time: 148 ms, Memory: 17032 KB -BaekjoonHub

---
+++ 백준/Bronze/7279. Autobusas/Autobusas.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val k = nextInt()
+    var s = 0
+    var m = 0
+    for(i in 0 until n) {
+        val a = nextInt()
+        val b = nextInt()
+        s += a-b
+        if(s-k >0 && s-k > m) {
+            m = s-k
+        }
+    }
+    print(m)
+}
