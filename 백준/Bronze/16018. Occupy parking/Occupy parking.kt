e398b53015bd2f6918305a65d33c129aa44c9701
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2024-12-28 19:23:37 +0900

    [Bronze III] Title: Occupy parking, Time: 100 ms, Memory: 13240 KB -BaekjoonHub

--- 백준/Bronze/16018. Occupy parking/Occupy parking.kt
+++ 백준/Bronze/16018. Occupy parking/Occupy parking.kt
@@ -5,8 +5,8 @@ fun main() = with(Scanner(System.`in`)) {
     val t = next()
     val y = next()
     var c = 0
-    for(i in 0 until n) {
-        if(t[i] == 'C' && t[i] == y[i]) c++
+    for (i in 0 until n) {
+        if (t[i] == 'C' && t[i] == y[i]) c++
     }
     print(c)
 }
