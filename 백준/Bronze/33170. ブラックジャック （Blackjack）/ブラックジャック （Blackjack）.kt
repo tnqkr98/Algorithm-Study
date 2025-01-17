d899e73f1f27ddc30b8f216847a0080436a9db6c
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2025-01-18 00:49:35 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 100 ms, Memory: 13304 KB -BaekjoonHub

--- 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.kt
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.kt
@@ -4,6 +4,6 @@ fun main() = with(Scanner(System.`in`)) {
     val a = nextInt()
     val b = nextInt()
     val c = nextInt()
-    if(a+b+c <= 21) print(1)
+    if (a + b + c <= 21) print(1)
     else print(0)
 }
