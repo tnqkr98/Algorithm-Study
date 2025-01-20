60fa3dbc2334c01da0983b729675dcb967f59f2e
Dong Han Park <48381543+tnqkr98@users.noreply.github.com>
2025-01-20 21:11:30 +0900

    [Bronze IV] Title: 散歩 (Walking), Time: 100 ms, Memory: 13240 KB -BaekjoonHub

--- 백준/Bronze/33162. 散歩 （Walking）/散歩 （Walking）.kt
+++ 백준/Bronze/33162. 散歩 （Walking）/散歩 （Walking）.kt
@@ -3,9 +3,9 @@ import java.util.*
 fun main() = with(Scanner(System.`in`)) {
     val x = nextInt()
     var s = 0
-    for(i in 1 .. x) {
-        if(i%2 == 1) s+=3
-        else s-=2
+    for (i in 1..x) {
+        if (i % 2 == 1) s += 3
+        else s -= 2
     }
     print(s)
 }
